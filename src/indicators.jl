d31320839c31332b7bf3d8a1079c2481e77f6e6d
untoreh <francesco.giannelli@yahoo.it>
2021-12-30 18:35:20 +0100

    fix @as_ts macro inputs

--- src/indicators.jl
+++ src/indicators.jl
@@ -4,6 +4,15 @@ using DataFramesMeta
 
 macro as_ts(df, c1, cols...)
     df = esc(df)
+    local columns
+    if cols[1] isa QuoteNode
+        columns = [c.value for c in cols]::Vector{Symbol}
+        @show columns
+        return
+    else
+        @assert size(cols, 1) === 1
+        columns = esc(cols[1])
+    end
     if length(cols) === 0
         quote
             TS(getproperty($df, $c1), $df.timestamp, $c1)
@@ -13,8 +22,8 @@ macro as_ts(df, c1, cols...)
             if !isdefined(Main, :type)
                 type = Float64
             end
-            idx = hasproperty($df, :timestamp) ? $df.timestamp : $(esc(:dates))
-            ns = vcat([$c1]::Vector{Symbol}, collect(c.value for c in $cols)::Vector{Symbol})
+            idx = hasproperty($df, :timestamp) ? $(df).timestamp : $(esc(:dates))
+            ns = vcat([$c1]::Vector{Symbol}, $columns::Vector{Symbol})
 	        TS(@to_mat(@view($df[:, ns])), idx, ns)
         end
     end
