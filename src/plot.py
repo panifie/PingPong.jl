58322232be61b0356c8e464e33e70be9c7d40e63
untoreh <francesco.giannelli@yahoo.it>
2022-01-03 17:37:29 +0100

    more plots

--- src/plot.py
+++ src/plot.py
@@ -12,22 +12,26 @@ from pyecharts.commons.utils import JsCode
 from pyecharts.charts import Kline, Line, Bar, Grid
 
 ECHART_OHLC_COLS = ["open", "close", "low", "high"]
-CHART_WIDTH="1920px"
-CHART_HEIGHT="1080px"
+CHART_WIDTH = "1920px"
+CHART_HEIGHT = "1080px"
+
 
 def render(chart):
     chart_path = str(getcwd() / Path("render.html"))
     print(f"Writing chart to {chart_path}")
     chart.render(chart_path)
 
+
 def volume_bar(x, y):
     b = Bar(init_opts=INIT_OPTS)
     b.add_xaxis(xaxis_data=x)
-    b.add_yaxis(series_name="",
-                # xaxis_index=1,
-                # yaxis_index=1,
-                y_axis=y,
-                label_opts=opts.LabelOpts(is_show=False))
+    b.add_yaxis(
+        series_name="",
+        # xaxis_index=1,
+        # yaxis_index=1,
+        y_axis=y,
+        label_opts=opts.LabelOpts(is_show=False),
+    )
     b.set_global_opts(
         xaxis_opts=opts.AxisOpts(
             type_="category",
@@ -41,21 +45,26 @@ def volume_bar(x, y):
         ),
         yaxis_opts=opts.AxisOpts(
             is_scale=True,
-            splitarea_opts=opts.SplitAreaOpts(is_show=True, areastyle_opts=opts.AreaStyleOpts(opacity=1)),
+            splitarea_opts=opts.SplitAreaOpts(
+                is_show=True, areastyle_opts=opts.AreaStyleOpts(opacity=1)
+            ),
             axislabel_opts=opts.LabelOpts(is_show=False),
         ),
         legend_opts=opts.LegendOpts(is_show=True, pos_left=0),
-        )
+    )
     return b
+
 
 def ind_bar(x, y, name="", grid_idx=1, legend_pos={}):
     b = Bar(init_opts=INIT_OPTS)
     b.add_xaxis(xaxis_data=x)
-    b.add_yaxis(series_name=name,
-                y_axis=y,
-                # xaxis_index=grid_idx,
-                # yaxis_index=grid_idx,
-                label_opts=opts.LabelOpts(is_show=False))
+    b.add_yaxis(
+        series_name=name,
+        y_axis=y,
+        # xaxis_index=grid_idx,
+        # yaxis_index=grid_idx,
+        label_opts=opts.LabelOpts(is_show=False),
+    )
     b.set_global_opts(
         xaxis_opts=opts.AxisOpts(
             type_="category",
@@ -70,12 +79,15 @@ def ind_bar(x, y, name="", grid_idx=1, legend_pos={}):
         yaxis_opts=opts.AxisOpts(
             grid_index=grid_idx,
             is_scale=True,
-            splitarea_opts=opts.SplitAreaOpts(is_show=True, areastyle_opts=opts.AreaStyleOpts(opacity=1)),
+            splitarea_opts=opts.SplitAreaOpts(
+                is_show=True, areastyle_opts=opts.AreaStyleOpts(opacity=1)
+            ),
             axislabel_opts=opts.LabelOpts(is_show=False),
         ),
         legend_opts=opts.LegendOpts(is_show=True, **legend_pos),
     )
     return b
+
 
 def ind_line(x, y, name="", grid_idx=1, legend_pos={}):
     l = Line(init_opts=INIT_OPTS)
@@ -139,17 +151,15 @@ def kline_chart(x, y, name=""):
     # )
     return k
 
+
 def k_chart_global_opts(chart, name, x_idx, grid_idx=0, y_idx=[]):
     if not y_idx:
         y_idx.extend(x_idx)
 
     chart.set_global_opts(
-        legend_opts=opts.LegendOpts(
-            is_show=True),
+        legend_opts=opts.LegendOpts(is_show=True),
         title_opts=opts.TitleOpts(title=name, pos_left="0"),
-        xaxis_opts=opts.AxisOpts(
-            grid_index=grid_idx
-            ),
+        xaxis_opts=opts.AxisOpts(grid_index=grid_idx),
         yaxis_opts=opts.AxisOpts(
             grid_index=grid_idx,
             is_scale=True,
@@ -164,51 +174,53 @@ def k_chart_global_opts(chart, name, x_idx, grid_idx=0, y_idx=[]):
             background_color="rgba(245, 245, 245, 0.8)",
             border_width=1,
             border_color="#ccc",
-            textstyle_opts=opts.TextStyleOpts(color="#000") ),
+            textstyle_opts=opts.TextStyleOpts(color="#000"),
+        ),
         visualmap_opts=opts.VisualMapOpts(
             is_show=False,
             dimension=2,
             series_index=5,
             is_piecewise=True,
             pieces=[
-                {"value" : 1, "color" : "#00da3c"},
-                {"value" : -1, "color" : "#ec0000"},
+                {"value": 1, "color": "#00da3c"},
+                {"value": -1, "color": "#ec0000"},
             ],
         ),
         axispointer_opts=opts.AxisPointerOpts(
             is_show=True,
-            link=[{"xAxisIndex" : "all"}],
+            link=[{"xAxisIndex": "all"}],
             label=opts.LabelOpts(background_color="#777"),
         ),
         brush_opts=opts.BrushOpts(
             x_axis_index="all",
             brush_link="all",
-            out_of_brush={"colorAlpha" : 0.1},
+            out_of_brush={"colorAlpha": 0.1},
             brush_type="lineX",
         ),
-datazoom_opts=[
-        opts.DataZoomOpts(
-            xaxis_index=x_idx,
-            is_show=False,
-            type_="inside",
-            range_start=98,
-            range_end=100
-        ),
-        opts.DataZoomOpts(
-            xaxis_index=y_idx,
-            is_show=True,
-            type_="slider",
-            pos_top="90%",
-            range_start=98,
-            range_end=100
-        ),
-    ]
-        )
+        datazoom_opts=[
+            opts.DataZoomOpts(
+                xaxis_index=x_idx,
+                is_show=False,
+                type_="inside",
+                range_start=98,
+                range_end=100,
+            ),
+            opts.DataZoomOpts(
+                xaxis_index=y_idx,
+                is_show=True,
+                type_="slider",
+                pos_top="90%",
+                range_start=98,
+                range_end=100,
+            ),
+        ],
+    )
 
-def grid(dates, ohlc, inds: Dict[str, Tuple[str, List]]= {}, name="OHLCV"):
-    '''
+
+def grid(dates, ohlc, inds: Dict[str, Tuple[str, List]] = {}, name="OHLCV"):
+    """
     `inds`: Every indicator should specify its plot type. Eg. ("bar", [...])
-    '''
+    """
 
     global INIT_OPTS
     INIT_OPTS = opts.InitOpts(width="1400px", height="800px")
@@ -219,7 +231,6 @@ def grid(dates, ohlc, inds: Dict[str, Tuple[str, List]]= {}, name="OHLCV"):
 
     iinds = inds.copy()
     del iinds["volume"]
-
 
     ic = None
     legend_pos = {"pos_left": 0, "pos_top": 0}
@@ -234,15 +245,17 @@ def grid(dates, ohlc, inds: Dict[str, Tuple[str, List]]= {}, name="OHLCV"):
             ind_charts.append(ic)
 
     k_chart.set_global_opts(
-           tooltip_opts=opts.TooltipOpts(
+        tooltip_opts=opts.TooltipOpts(
             trigger="axis",
             axis_pointer_type="cross",
             background_color="rgba(245, 245, 245, 0.8)",
             border_width=1,
             border_color="#ccc",
-            textstyle_opts=opts.TextStyleOpts(color="#000") ))
+            textstyle_opts=opts.TextStyleOpts(color="#000"),
+        )
+    )
     # NOTE: Options need to be applied before adding charts to the grid (They are copied?)
-    zoom_idx = list(range(2+len(ind_charts))) if len(ind_charts) else [0, 1]
+    zoom_idx = list(range(2 + len(ind_charts))) if len(ind_charts) else [0, 1]
     k_chart_global_opts(k_chart, "ohlc", x_idx=zoom_idx, grid_idx=0)
     # NOTE: The order is importat for correct application of global options (like zoom)
     g.add(k_chart, grid_opts=opts.GridOpts(height="49%"))
@@ -253,31 +266,33 @@ def grid(dates, ohlc, inds: Dict[str, Tuple[str, List]]= {}, name="OHLCV"):
 
     render(g)
 
-def scatter3d(data: dict, name="", x_name="x", y_name="y", z_name="z"):
+
+def scatter3d(data: dict, name="", x="x", y="y", z="z",):
     sc = pyecharts.charts.Scatter3D(init_opts=INIT_OPTS)
-    sc.add(series_name=name,
-           data=data,
-           xaxis3d_opts=opts.Axis3DOpts(
-               name=x_name,
-               type_="value"
-           ),
-           yaxis3d_opts=opts.Axis3DOpts(
-               name=y_name,
-               type_="value"
-           ),
-           zaxis3d_opts=opts.Axis3DOpts(
-               name=z_name,
-               type_="value"
-           ),
-           grid3d_opts=opts.Grid3DOpts(
-               width=100,
-               height=100,
-               depth=100
-               ))
+    sc.add(
+        series_name=name,
+        data=data,
+        xaxis3d_opts=opts.Axis3DOpts(name=x, type_="value"),
+        yaxis3d_opts=opts.Axis3DOpts(name=y, type_="value"),
+        zaxis3d_opts=opts.Axis3DOpts(name=z, type_="value"),
+        grid3d_opts=opts.Grid3DOpts(width=100, height=100, depth=100),
+    )
+    sc.set_global_opts(
+        visualmap_opts=[
+            # opts.VisualMapOpts(
+            #     type_="size",
+            #     is_calculable=True,
+            #     is_show=True,
+            #     # range_size=range(1, 200),
+            #     dimension=2,
+            #     range_opacity=0.2
+            # ),
+            opts.VisualMapOpts(
+                type_="color", is_calculable=True, is_show=True, dimension=1
+            ),
+        ],
+    )
     render(sc)
 
 
-plotsfn = {
-    "bar": ind_bar,
-    "line": ind_line
-}
+plotsfn = {"bar": ind_bar, "line": ind_line}
