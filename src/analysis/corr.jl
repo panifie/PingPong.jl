04cee09c9962f2c41071659883dd26b5f3a61c24
untoreh <francesco.giannelli@yahoo.it>
2022-01-09 17:32:14 +0100

    add corr metrics

--- src/analysis/corr.jl
+++ src/analysis/corr.jl
@@ -1,9 +1,8 @@
-using Backtest.Misc: setopt!
-using StatsBase: corspearman, corkendall, corkendall!
+using Backtest.Misc: setopt!, options
+using StatsBase: corspearman, corkendall, corkendall!, mean
 using StatsModels: lag, lead
 using EffectSizes: effectsize, CohenD, HedgeG, GlassΔ
-using CausalityTools: transferentropy, RectangularBinning
-import CausalityTools
+import CausalityTools; const ct = CausalityTools; const di = ct.SMeasure.Distances;
 # import StatsBase: corspearman
 
 @inline _x_view(x, n_lead) = @view(x[begin:end-n_lead])
@@ -50,24 +49,70 @@ function qcorr(x::AbstractArray, y::AbstractArray, n_lead=1, default=NaN)
     (mxy + myx - 2) / ((mxy * myx) - 1)
 end
 
-setopt!("tentr",
-        (;est=CausalityTools.SymbolicPermutation(; τ=100, lt=CausalityTools.Entropies.isless_rand),
-            # CausalityTools.KozachenkoLeonenko(10, 2),
-            # CausalityTools.TransferOperator(RectangularBinning(2)),
-            # VisitationFrequency(RectangularBinning(2)),
-         base=2,
-         q=1.))
+const est = Dict()
+function map_estimators!()
+    est[:vf] = ct.VisitationFrequency(ct.RectangularBinning(2))
+    est[:sp] = ct.SymbolicPermutation()
+    est[:to] = ct.TransferOperator(ct.RectangularBinning(2))
+    est[:kl] = ct.KozachenkoLeonenko(10, 2)
+    est[:hb] = ct.Hilbert(est[:vf])
+end
+map_estimators!()
+
+# @show CausalityTools.SymbolicWeightedPermutation(;m=3)
+setopt!("ct",
+        Dict(
+            :tentr => (;est=est[:vf], base=2, q=1.),
+            :muten => (;est=est[:vf]),
+            :predas => (;est=est[:to], ηs=2),
+            :pai => (;d=2, τ=1, w=2),
+            :cross => (;d=1, τ=1),
+            :smeas => (;k=2),
+            :joindd => (;dm=di.SqEuclidean(), B=2, D=2, τ=1)
+        ))
 
 function tentr(x::AbstractArray, y::AbstractArray, args...)
-    opt = options["tentr"]
-    transferentropy(x, selectdim(y, 1, :), opt.est;
+    opt = options["ct"][:tentr]
+    ct.transferentropy(x, y, opt.est;
                     base=opt.base,
                     q=opt.q)
 end
 
+function muten(x::AbstractArray, y::AbstractArray, args...)
+    opt = options["ct"][:muten]
+    ct.mutualinfo(x, y, opt.est;)
+end
+
+function predas(x::AbstractArray, y::AbstractArray, args...)
+    opt = options["ct"][:predas]
+    ct.predictive_asymmetry(y, x, opt.est, opt.ηs)
+end
+
+function cross(x::AbstractArray, y::AbstractArray, n_lead=1, default=NaN)
+    opt = options["ct"][:cross]
+    ct.CrossMappings.pai(x, y, opt.d, opt.τ)
+end
+
+function pai(x::AbstractArray, y::AbstractArray, args...)
+    opt = options["ct"][:pai]
+    ct.CrossMappings.pai(x, y, opt.d, opt.τ, :segment) |> mean
+end
+
+function smeas(x::AbstractArray, y::AbstractArray, n_lead=1, default=NaN)
+    opt = options["ct"][:smeas]
+    # FIXME: providing k as kwarg, has wrong method signature and doesn't dispatch
+    ct.s_measure(_x_view(x, n_lead), _y_view(y, n_lead, default); metric=di.CosineDist())
+end
+
+# NOTE: this is heavy
+function joindd(x::AbstractArray, y::AbstractArray, args...)
+    opt = options["ct"][:joindd]
+    ct.jdd(x, y; distance_metric=opt.dm, B=opt.B, D=opt.D, τ=opt.τ) |> mean
+end
+
 macro _wrap_funcs()
     quote
-        for f in [:corsp, :corke, :cohen, :hedge, :glass, :qcorr, :tentr]
+        for f in [:corsp, :corke, :cohen, :hedge, :glass, :qcorr, :tentr, :muten, :predas, :cross, :pai, :smeas, :joindd]
             @_wrap_df_fun f
         end
     end
