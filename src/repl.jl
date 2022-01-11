e5bfe9be8ca44a6a92ae3060857604b9dc295ae9
untoreh <francesco.giannelli@yahoo.it>
2022-01-11 12:32:56 +0100

    repl funcs

--- src/repl.jl
+++ src/repl.jl
@@ -1,6 +1,10 @@
 macro in_repl()
     quote
-        @eval an = Backtest.Analysis
+        @eval begin
+            an = Backtest.Analysis
+            using Backtest.Plotting: plotone, @plotone
+            using Backtest.Misc: options, @margin!, @margin!!
+        end
         copy!(exc, Backtest.Exchanges.getexchange(:kucoin))
         exckeys!(exc, values(Backtest.Exchanges.kucoin_keys())...)
         zi = ZarrInstance()
