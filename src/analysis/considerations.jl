9869cc0cc2462c9705274796c157f36b68588d03
untoreh <francesco.giannelli@yahoo.it>
2022-01-31 18:13:07 +0100

    stage2 indicator fixes
    - Fix normalization since the unitrange transform is not between 0-1

--- src/analysis/considerations.jl
+++ src/analysis/considerations.jl
@@ -71,7 +71,7 @@ istennisball(df::AbstractDataFrame; kwargs...) = istennisball(df.low; kwargs...)
 # STAGE2
 
 @doc "Since relative strength computes the score for the whole set, it is useful to cache it when querying elements from the same set."
-const rs_cache = Ref(hash(0) => DataFrame())
+const rs_cache = Ref(hash(0) => (DataFrame(), Matrix{Float64}(undef, 0, 0)))
 
 normalize!(arr; unit = false, dims = ndims(arr)) = _normalize(arr; unit, dims, copy = true)
 normalize(arr; unit = false, dims = ndims(arr)) = _normalize(arr; unit, dims, copy = false)
@@ -86,7 +86,7 @@ end
 Given a collection of price timeseries (1D), calculate the _relative strength_ of each series
 as the summation of the rate of change at each timestep _t_.
 """
-@views function relative_strength(price_mat::Matrix, vol_mat::Matrix; norm_roc = false, norm_str = false)
+@views function relative_strength(price_mat::Matrix, vol_mat::Matrix; norm_roc = false)
     @assert size(price_mat) == size(vol_mat)
     # normalize volume pair historically (not across all pairs).
     norm_vol = normalize(vol_mat[2:end, :]; unit = true, dims = 1)
@@ -96,10 +96,10 @@ as the summation of the rate of change at each timestep _t_.
     norm_roc && normalize!(roc; dims = 2)
     # # weighting by the historically normalized pair volume
     roc .*= norm_vol
-    s = sum(roc; dims = 1)
-    s = reshape(s, size(s, 2))
-    norm_str && normalize!(s; unit = true)
-    s
+    # rel_str = norm_vol
+    # cumsum!(rel_str, roc; dims = 1)
+    rel_str = cumsum(roc; dims = 1)
+    rel_str
 end
 
 function mrkts_key(mrkts::AbstractDict; kwargs...)
@@ -111,24 +111,28 @@ end
 @doc """Computes the relative strength of a collection (dict) of pairs. Returning the queried one, and the rest.
 `sorted`: Sort the matrix of all relative strengths.
 `norm_roc`: Smooths the ranking by normalizing the rate of change at each candle.
-`norm_str`: Normalizes the relative strength between 0 and 1.
 """
-function relative_strength(pair::AbstractString, mrkts::AbstractDict; sorted = true, norm_roc = false, norm_str = false)
+function relative_strength(pair::AbstractString, mrkts::AbstractDict; sorted = true, norm_roc = false)
     @assert pair ∈ keys(mrkts) "Pair not present in data dict."
-    mk = mrkts_key(mrkts; sorted, norm_roc, norm_str)
+    @as_dfdict mrkts
+    mk = mrkts_key(mrkts; sorted, norm_roc)
     local df
     if rs_cache[][1] === mk
-        df = rs_cache[][2]
+        df = rs_cache[][2][1]
+        rs = rs_cache[][2][2]
     else
-        @as_dfdict mrkts
         price_mat, vol_mat, pairs = pair_matrix(mrkts)
-        rs = relative_strength(price_mat, vol_mat; norm_roc, norm_str)
-        rs = hcat(pairs, rs)
-        df = DataFrame(rs, [:pair, :rs])
+        rs = relative_strength(price_mat, vol_mat; norm_roc)
+        last_rs = reshape(@view(rs[end, :]), size(rs, 2))
+        mn, mx = extrema(last_rs)
+        last_rs[:] = (last_rs .- mn) ./ (mx - mn)
+        data = hcat(pairs, last_rs)
+        df = DataFrame(data, [:pair, :rs])
         sorted && sort!(df, :rs)
-        rs_cache[] = mk => df
+        rs_cache[] = mk => (df, rs)
     end
-    (df[firststring(pair, df.pair), :rs], df)
+    pair_idx = firststring(pair, df.pair)
+    (df[pair_idx, :rs], df, @view(rs[:, pair_idx]))
 end
 
 function firststring(str, arr)
@@ -156,16 +160,16 @@ end
 # speedup concatenation
 catmarkets(mrkts::Vector) = reduce(hcat, mrkts)
 
-s2_weights = (
-    isabove = 1,
-    isabove2 = 1,
-    is_trending = 1,
-    isaboveyear = 1,
-    isclosehigh = 1,
-    isabove50 = 1,
-    outofbase = 1,
-    rel_str = 1
-)
+const s2_weights = Ref((
+    isaboveprice = 0.05,
+    isabove150 = 0.05,
+    isabove50200 = 0.1,
+    is_trending = 0.1,
+    isaboveyear = 0.15,
+    isclosehigh = 0.15,
+    rel_str = 0.3,
+    isabove50base = 0.1
+))
 
 @doc """
 Stage 2 template.
@@ -177,33 +181,46 @@ Stage 2 template.
     low::AbstractVector,
     close::AbstractVector,
     mrkts::AbstractDict;
-    base° = 5, baseσ = 15, norm_roc = false)
+    base° = 5, baseσ = 30, norm_roc = false)
     @assert length(close) > 365 "Stage 2 indicator needs at least 365 candles."
     price = close[end]
     μ150 = mean(close[end-150+1:end])
     μ200 = mean(close[end-200+1:end])
-    isabove = price > μ150 && price > μ200
-    μ50 = mean(close[end-50+1:end])
-    isabove2 = μ50 > μ150 && μ50 > μ200
+    # 1
+    isaboveprice = price > μ150 && price > μ200
+    # 2
+    isabove150 = μ150 > μ200
+    # 3
     ma200 = ind.sma(close[end-230-1:end]; n = 200)[end-30+1:end]
     is_trending = (ma200[2:end] .> ma200[1:end-1]) |> all
+    # 4
+    μ50 = mean(close[end-50+1:end])
+    isabove50200 = μ50 > μ150 && μ50 > μ200
+    # 5
     year_low = low[end-364-1:end]
     isaboveyear = price > minimum(year_low) * 1.25
+    # 6
     year_high = high[end-364-1:end]
     isclosehigh = price > maximum(year_high) * 0.75
+    # 7
+    pair_rs, _, rs_ts = relative_strength(pair, mrkts; norm_roc)
+    is_uptrend_rs = (slopeangle(rs_ts[end-84-1:end], n=42) .> 0) |> all
+    rel_str = pair_rs > 0.8 && is_uptrend_rs
+    # 8
     days50 = close[end-50+1:end]
     μ50 = mean(days50)
-    isabove50 = price > μ50
     outofbase = (slopeangle(days50; n = baseσ)[baseσ+1:end] .< base°) |> all
-    rel_str = relative_strength(pair, mrkts; norm_str = true, norm_roc)[1] > 0.8
-    vars = (; isabove, isabove2, is_trending, isaboveyear, isclosehigh, isabove50, outofbase, rel_str)
-    (;vars..., score=_score_sum(vars; weights = s2_weights))
+    isabove50base = outofbase && price > μ50
+
+    # Weights
+    vars = (; isaboveprice, isabove150, isabove50200, is_trending, isaboveyear, isclosehigh, rel_str, isabove50base)
+    (; vars..., score = _score_sum(vars; weights = s2_weights[]))
 end
 
 function stage2(mrkts::AbstractDict; sorted = true, kwargs...)
     @as_dfdict mrkts
     df = _stage2(mrkts; kwargs...)
-    sorted && sort!(df, :score)
+    sorted && !isempty!(df) && sort!(df, :score)
     df
 end
 
@@ -221,17 +238,17 @@ stage2(mrkts, tfs::Vector{String}; kwargs...) = maptf(tfs, mrkts, stage2; kwargs
 
 # /STAGE2
 
-cweights = (
+const cweights = Ref((
     ft = 0.15,
     up = 0.05,
     bvol = 0.25,
     tball = 0.3,
     mvp = 0.25
-)
+))
 
 @doc "Evaluate trais for a single pair."
 function considerations(df::AbstractDataFrame; window = 20, window2 = 50,
-    min_follow::Int = 1, vol_thresh = 0.1, snapback = 3, weights = cweights, mvpargs = (;))
+    min_follow::Int = 1, vol_thresh = 0.1, snapback = 3, weights = cweights[], mvpargs = (;))
     @debug @assert size(df, 1) > window2
 
     dfv = @view df[end-window:end, :]
@@ -250,7 +267,7 @@ end
 _trueish(syms...) = all(isnothing(sym) || sym for sym in syms)
 
 @doc "Evaluate traits for a collection of pairs."
-function considerations(mrkts::AbstractDict; all = false, window = 20, window2 = 50, kwargs...)
+function considerations(mrkts::AbstractDict; all = false, sorted=true, window = 20, window2 = 50, kwargs...)
     local df
     kargs = (; window, window2, kwargs...)
     if valtype(mrkts) <: PairData
@@ -260,7 +277,8 @@ function considerations(mrkts::AbstractDict; all = false, window = 20, window2 =
     all && @rsubset! df begin
         _trueish(:ft, :up, :bvol, :tball)
     end
-    sort!(df, :score)
+    sorted && !isempty(df) && sort!(df, :score)
+    df
 end
 
 function _considerations(mrkts::AbstractDict{String,DataFrame}; kwargs...)
@@ -274,6 +292,6 @@ end
 @doc "Evaluate traits on multiple timeframes."
 considerations(mrkts, tfs::Vector{String}; kwargs...) = maptf(tfs, mrkts, considerations; kwargs...)
 
-export considerations
+export considerations, stage2
 
 end
