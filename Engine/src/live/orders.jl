8bc17ffa87b5caeec3622d298cb72f7077a024c7
untoreh <francesco.giannelli@yahoo.it>
2023-01-11 16:41:07 +0100

    docs: explain liveorder date

--- Engine/src/live/orders.jl
+++ Engine/src/live/orders.jl
@@ -6,6 +6,22 @@ using ..Instances: AssetInstance
 @doc """A live order tracks in flight trades.
 
  `date`: the time at which the strategy requested the order.
+         The strategy is assumed to have *knowledge* of the ohlcv data \
+         strictly lower than the timeframe adjusted date.
+         Example:
+        ```julia
+        ts = dt"2020-05-24T02:34:00" # the date of the order request
+        tf = @infertf ohlcv # get the timeframe (15m)
+        start_date = ohlcv.timestamp[begin]
+        stop_date = apply(tf, ts) # normalize date to timeframe
+        stop_date -= tf.period # scale down by one timeframe step
+        # At this point the stop date would be `2020-05-24T02:30:00`
+        # which covers the period between ...02:30:00..02:45:00...
+        # Therefore the strategy can only have access to data < 02:30:00
+        avail_ohlcv = ohlcv[DateRange(start_date, stop_date), :]
+        @assert isless(avail_ohlcv.timestamp[end], dt"2020-05-24T02:30:00")
+        @assert isequal(avail_ohlcv.timestamp[end] + tf.period, dt"2020-05-24T02:30:00")
+        ```
  `delay`: how much time has passed since the order request \
           and the exchange execution of the order (to account for api issues).
  """
