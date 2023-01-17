8f3a4ed7ec685ad0f81842de19276d6abda801cc
untoreh <francesco.giannelli@yahoo.it>
2023-01-17 14:51:22 +0100

    feat: derivatives predicates

--- Pairs/src/derivatives.jl
+++ Pairs/src/derivatives.jl
@@ -4,7 +4,7 @@ using ..Pairs: FULL_SYMBOL_GROUPS_REGEX
 
 @doc "A symbol parsed as settlement currency."
 const SettlementCurrency = Symbol
-@enum DerivativeKind Perp Call Put
+@enum DerivativeKind Unkn Call Put
 parse_option(s::AbstractString) = begin
     s == "C" && return Call
     s == "P" && return Put
@@ -32,7 +32,7 @@ struct Derivative1{A<:Asset}
         S = Symbol(m[3])
         id = isnothing(m[4]) ? SubString("") : m[4]
         strike = isnothing(m[5]) || isempty(m[5]) ? 0.0 : parse(Float64, m[5])
-        kind = isnothing(m[6]) || isempty(m[6]) ? Perp : parse_option(m[6])
+        kind = isnothing(m[6]) || isempty(m[6]) ? Unkn : parse_option(m[6])
         Derivative1(asset, S, id, strike, kind)
     end
 end
@@ -42,6 +42,20 @@ import Base.getproperty
 function getproperty(d::Derivative, s::Symbol)
     s ∈ fieldnames(Asset) && return getproperty(getfield(d, :asset), s)
     getfield(d, s)
+end
+
+@inline begin
+    @doc "Predicates according to [OctoBot](https://github.com/Drakkar-Software/OctoBot-Commons/blob/master/octobot_commons/symbols/symbol.py)"
+    is_settled(d::Derivative) = d.sc != Symbol()
+    has_strike(d::Derivative) = d.strike != 0.0
+    expires(d::Derivative) = !isempty(d.id)
+    is_future(d::Derivative) = is_settled(d) && !has_strike(d) && d.kind == Unkn
+    is_perp(d::Derivative) = is_future(d) && !expires(d.id)
+    is_spot(d::Derivative) = !is_settled(d)
+    is_option(d::Derivative) =
+        d.kind != Unkn && is_settled(d) && has_strike(d) && expires(d)
+    is_linear(d::Derivative) = is_settled(d) ? d.qc == d.sc : true
+    is_inverse(d::Derivative) = is_settled(d) ? d.bc == d.sc : false
 end
 
 export Derivative, DerivativeKind
