c9696e887b66b619361ce2715a41f742e3e6aa9f
untoreh <francesco.giannelli@yahoo.it>
2023-01-18 14:58:27 +0100

    fix: is_last_complete_candle args

--- Analysis/Processing/src/ohlcv.jl
+++ Analysis/Processing/src/ohlcv.jl
@@ -1,22 +1,23 @@
 using DataFramesMeta
 using Dates: Period, now, UTC
-using Misc: @as_td, dt, Candle
+using TimeTicks
+using Misc: Candle
 using Data: to_df
 
 @doc """Assuming timestamps are sorted, returns a new dataframe with a contiguous rows based on timeframe.
 Rows are filled either by previous close, or NaN. """
-fill_missing_rows(df, timeframe::AbstractString; strategy = :close) = begin
+fill_missing_rows(df, timeframe::AbstractString; strategy=:close) = begin
     @as_td
-    _fill_missing_rows(df, prd; strategy, inplace = false)
+    _fill_missing_rows(df, prd; strategy, inplace=false)
 end
 
-fill_missing_rows!(df, prd::Period; strategy = :close) = begin
-    _fill_missing_rows(df, prd; strategy, inplace = true)
+fill_missing_rows!(df, prd::Period; strategy=:close) = begin
+    _fill_missing_rows(df, prd; strategy, inplace=true)
 end
 
-fill_missing_rows!(df, timeframe::AbstractString; strategy = :close) = begin
+fill_missing_rows!(df, timeframe::AbstractString; strategy=:close) = begin
     @as_td
-    _fill_missing_rows(df, prd; strategy, inplace = true)
+    _fill_missing_rows(df, prd; strategy, inplace=true)
 end
 
 function _fill_missing_rows(df, prd::Period; strategy, inplace)
@@ -48,7 +49,7 @@ end
 @doc """Similar to the freqtrade homonymous function.
 - `fill_missing`: `:close` fills non present candles with previous close and 0 volume, else with `NaN`.
 """
-function cleanup_ohlcv_data(data, timeframe; col = 1, fill_missing = :close)
+function cleanup_ohlcv_data(data, timeframe; col=1, fill_missing=:close)
     @debug "Cleaning dataframe of size: $(size(data, 1))."
     size(data, 1) === 0 && return _empty_df()
     df = data isa DataFrame ? data : to_df(data)
@@ -67,7 +68,7 @@ function cleanup_ohlcv_data(data, timeframe; col = 1, fill_missing = :close)
     # delete!(df, timefloat.(df.timestamp) .% td .!== 0.)
     # @debug "DataFrame without bad timestamp size: $(size(df, 1))"
 
-    gd = groupby(df, :timestamp; sort = true)
+    gd = groupby(df, :timestamp; sort=true)
     df = combine(
         gd,
         :open => first,
@@ -75,7 +76,7 @@ function cleanup_ohlcv_data(data, timeframe; col = 1, fill_missing = :close)
         :low => minimum,
         :close => last,
         :volume => maximum;
-        renamecols = false,
+        renamecols=false,
     )
 
     if is_incomplete_candle(df[end, :], td)
@@ -84,29 +85,28 @@ function cleanup_ohlcv_data(data, timeframe; col = 1, fill_missing = :close)
         @debug "Dropping last candle ($(last_candle[:timestamp] |> string)) because it is incomplete."
     end
     if fill_missing !== false
-        fill_missing_rows!(df, prd; strategy = fill_missing)
+        fill_missing_rows!(df, prd; strategy=fill_missing)
     end
     df
 end
 
 @doc "Checks if a candle timestamp is too new."
-function is_incomplete_candle(ts::AbstractFloat, td::AbstractFloat)
+@inline function is_incomplete_candle(ts::F, td::F) where {F<:AbstractFloat}
     nw = timefloat(now(UTC))
     ts + td > nw
 end
 
-function is_incomplete_candle(x::String, td::AbstractFloat)
+@inline function is_incomplete_candle(x::String, td::AbstractFloat)
     ts = timefloat(x)
     is_incomplete_candle(ts, td)
 end
 
-function is_incomplete_candle(candle, td::AbstractFloat)
+@inline function is_incomplete_candle(candle, td::AbstractFloat)
     ts = timefloat(candle.timestamp)
-    nw = timefloat(now(UTC))
-    ts + td > nw
+    is_incomplete_candle(ts, td)
 end
 
-function is_incomplete_candle(x, timeframe = "1m")
+@inline function is_incomplete_candle(x, timeframe="1m")
     @as_td
     is_incomplete_candle(x, td)
 end
