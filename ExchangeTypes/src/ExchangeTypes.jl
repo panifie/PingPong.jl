01a105929b73b546a1925b1f37c1b3b2664b504e
untoreh <francesco.giannelli@yahoo.it>
2023-01-20 11:32:15 +0100

    docs(exchanges)

--- ExchangeTypes/src/ExchangeTypes.jl
+++ ExchangeTypes/src/ExchangeTypes.jl
@@ -5,6 +5,7 @@ using FunctionalCollections
 using Ccxt: ccxt
 using Lang: Option
 
+@doc "An ExchangeID is a symbol checked to match a ccxt exchange class."
 struct ExchangeID{I}
     ExchangeID(sym::Symbol=Symbol()) = begin
         sym == Symbol() && return new{sym}()
@@ -49,6 +50,7 @@ Base.Broadcast.broadcastable(q::ExchangeID) = Ref(q)
 import Base.==
 ==(id::ExchangeID, s::Symbol) = Base.isequal(nameof(id), s)
 
+@doc "Same as ccxt precision mode enums."
 @enum ExcPrecisionMode excDecimalPlaces = 2 excSignificantDigits = 3 excTickSize = 4
 
 const OptionsDict = Dict{String,Dict{String,Any}}
@@ -66,12 +68,19 @@ struct Exchange8{I<:ExchangeID}
         new{typeof(id)}(x, id, name, [excDecimalPlaces], Set(), Dict())
     end
 end
+@doc """The exchange type wraps a ccxt exchange instance. Some attributes frequently accessed
+are copied over to avoid round tripping python. More attributes might be added in the future.
+To instantiate an exchange call `getexchange!` or `setexchange!`.
+
+"""
 Exchange = Exchange8
 
 Base.isempty(e::Exchange) = nameof(e.id) === Symbol()
 
 @doc "The hash of an exchange object is reduced to its symbol (the function used to instantiate the object from ccxt)."
 Base.hash(e::Exchange, u::UInt) = Base.hash(e.id, u)
+
+@doc "Attributes not matching the `Exchange` struct fields are forwarded to the wrapped ccxt class instance."
 function Base.getproperty(e::Exchange, k::Symbol)
     if hasfield(Exchange, k)
         if k == :precision
@@ -85,6 +94,7 @@ function Base.getproperty(e::Exchange, k::Symbol)
     end
 end
 
+@doc "Updates the global exchange `exc` variable."
 globalexchange!(new::Exchange) = begin
     global exc
     exc = new
@@ -92,9 +102,12 @@ globalexchange!(new::Exchange) = begin
 end
 
 
-@doc "Global implicit exchange instance."
+@doc "Global var implicit exchange instance.
+
+When working interactively, a global `exc` variable is available, updated through `globalexchange!`, which
+is used as the default for some functions when the exchange argument is omitted."
 exc = Exchange(pynew())
-@doc "Global holding Exchange instances to avoid dups."
+@doc "Global var holding Exchange instances. Used as a cache."
 const exchanges = Dict{Symbol,Exchange}()
 
 Base.display(exc::Exchange) = begin
