14136d91670c83541e5e430501f4f08793dc0a1d
untoreh <francesco.giannelli@yahoo.it>
2023-02-21 17:23:53 +0100

    func:

--- Misc/src/helpers.jl
+++ Misc/src/helpers.jl
@@ -21,3 +21,18 @@ function isdirempty(path::T where {T})
     allpaths = collect(walkdir(path))
     length(allpaths) == 1 && isempty(allpaths[1][2]) && isempty(allpaths[1][3])
 end
+
+@doc "Returns the range index of sorted vector `v` for all the values after `d`.
+when `strict` is false, the range will start after only the first occurence of `d`."
+function rangeafter(v::AbstractVector, d; strict=true, kwargs...)
+    r = searchsorted(v, d; kwargs...)
+    from = if length(r) > 0
+        ifelse(strict, r.start + length(r), r.start + 1)
+    else
+        r.start
+    end
+    from:lastindex(v)
+end
+
+@doc "Returns a view of the sorted vector `v`, indexed using `rangeafter`."
+after(v::AbstractVector, d) = view(v, rangeafter(v, d))
