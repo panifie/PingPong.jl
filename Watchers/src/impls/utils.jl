e8e83c2fb5af64c5550bc0eaaa5dc44784f46bdb
untoreh <francesco.giannelli@yahoo.it>
2023-02-27 09:21:50 +0100

    feat: Added generic functions for candles
    allow to fetch and resolve candles timeseries discrepancies

--- Watchers/src/impls/utils.jl
+++ Watchers/src/impls/utils.jl
@@ -1,5 +1,3 @@
-@define_fromdict!(true)
-
 _parsedatez(s::AbstractString) = begin
     s = rstrip(s, 'Z')
     Base.parse(DateTime, s)
@@ -10,7 +8,7 @@ macro parsedata(tick_type, mkts, key="symbol")
     mkts = esc(mkts)
     quote
         NamedTuple(
-            convert(Symbol, m[$key]) => @fromdict($tick_type, String, m) for m in $mkts
+            convert(Symbol, m[$key]) => fromdict($tick_type, String, m) for m in $mkts
         )
     end
 end
@@ -33,7 +31,7 @@ end
 macro append_dict_data(dict, data, maxlen_var)
     maxlen = esc(maxlen_var)
     quote
-        let function doappend((key, newdata))
+        let doappend((key, newdata)) = begin
                 df = @kget! $(esc(dict)) key DataFrame(; copycols=false)
                 appendmax!(df, newdata, $maxlen)
             end
@@ -42,13 +40,19 @@ macro append_dict_data(dict, data, maxlen_var)
     end
 end
 
+# FIXME
 Base.convert(::Type{Symbol}, s::LazyJSON.String) = Symbol(s)
 Base.convert(::Type{DateTime}, s::LazyJSON.String) = _parsedatez(s)
 Base.convert(::Type{DateTime}, s::LazyJSON.Number) = unix2datetime(s)
 Base.convert(::Type{String}, s::Symbol) = string(s)
 Base.convert(::Type{Symbol}, s::AbstractString) = Symbol(s)
 
-_check_contig(w, df) = _contiguous_ts(df.timestamp, timefloat(_tfr(w)))
+_checks(w) = w.attrs[:checks]
+_checksoff!(w) = w.attrs[:checks] = Val(:off)
+_checkson!(w) = w.attrs[:checks] = Val(:on)
+_do_check_contig(w, df, ::Val{:on}) = _contiguous_ts(df.timestamp, timefloat(_tfr(w)))
+_do_check_contig(_, _, ::Val{:off}) = nothing
+_check_contig(w, df) = !isempty(df) && _do_check_contig(w, df, _checks(w))
 
 _tfunc(w::Watcher) = w.attrs[:tfunc]
 _exc(w::Watcher) = w.attrs[:exc]
@@ -63,6 +67,8 @@ _lastdate(df::DataFrame) = df[end, :timestamp]
 _nextdate(df::DataFrame, tf) = df[end, :timestamp] + period(tf)
 _lastdate(z::ZArray) = z[end, 1] # the first col is a timestamp
 _curdate(tf) = apply(tf, now())
+_nextdate(tf) = _curdate(tf) + tf
+_dateidx(tf, from, to) = max(1, (to - from) ÷ period(tf))
 
 struct Warmed end
 struct Pending end
@@ -83,7 +89,8 @@ _warmed!(w, ::Pending) = w.attrs[:status] = Warmed()
 _pending!(w::Watcher) = w.attrs[:status] = Pending()
 _status(w::Watcher) = w.attrs[:status]
 _chill!(w) = w.attrs[:next] = apply(_tfr(w), now())
-_warmup!(w, ::Warmed) = nothing
+_warmup!(_, ::Warmed) = nothing
+@doc "Checks if we can start processing data, after we are past the initial incomplete timeframe."
 function _warmup!(w, ::Pending)
     apply(_tfr(w), now()) > w.attrs[:next] && _warmed!(w, _status(w))
 end
@@ -94,7 +101,84 @@ end
 
 _key!(w, k) = w.attrs[:key] = k
 _view!(w, v) = w.attrs[:view] = v
+_view(w) = w.attrs[:view]
 
-function _fetch_candles(w, from, to="")
-    fetch_candles(_exc(w), _tfr(w), _sym(w); zi=zilmdb(), from, to)
+function _fetch_candles(w, from, to="", sym=_sym(w))
+    fetch_candles(_exc(w), _tfr(w), sym; zi=zilmdb(), from, to)
+end
+
+function _fetch_error(w, from, to, sym=_sym(w))
+    error("Trades/ohlcv fetching failed for $sym @ $(_exc(w).name) from: $from to: $to")
+end
+
+_op(::Val{:append}, args...; kwargs...) = appendmax!(args...; kwargs...)
+_op(::Val{:prepend}, args...; kwargs...) = prependmax!(args...; kwargs...)
+_fromto(to, tf, cap, kept) = to - period(tf) * (cap - kept) - (2period(tf))
+function _from(df, to, tf, cap, ::Val{:append})
+    @debug @assert to >= _lastdate(tf)
+    kept = isempty(df) ? 0 : _lastdate(df) - to + period(tf), period(tf)
+    _fromto(to, tf, cap, kept)
+end
+_from(df, to, tf, cap, ::Val{:prepend}) = _fromto(to, tf, cap, nrow(df))
+
+@doc "`op`: `appendmax!` or `prependmax!`"
+function _fetchto!(w, df, sym, tf, op=Val(:append); to, from=nothing)
+    rows = nrow(df)
+    rows > 0 && _check_contig(w, df)
+    from = @something from _from(df, to, tf, w.capacity.view, op)#  if isempty(df)
+    if (to - from).value > 0
+        candles = _fetch_candles(w, from, to, sym) # add one more period to ensure from inclusion
+        from_to_range = rangebetween(candles.timestamp, from, to)
+        isempty(from_to_range) && _fetch_error(w, from, to, sym)
+        @debug begin
+            to,
+            candles[end, :timestamp],
+            from,
+            candles[begin, :timestamp],
+            length(from_to_range),
+            nrow(candles)
+        end
+        sliced = if length(from_to_range) == nrow(candles)
+            candles
+        else
+            view(candles, from_to_range, :)
+        end
+        @debug sliced[begin, :timestamp]
+        cleaned = cleanup_ohlcv_data(sliced, tf)
+        _firstdate(cleaned) != from + period(tf) && _fetch_error(w, from, to, sym)
+        _op(op, df, cleaned, w.capacity.view)
+        @debug @assert nrow(df) <= w.capacity.view
+    end
+end
+
+function _resolve(w, ohlcv_dst, ohlcv_src::DataFrame, sym=_sym(w))
+    _resolve(w, ohlcv_dst, _firstdate(ohlcv_src), sym)
+    _append_ohlcv!(
+        w, ohlcv_dst, ohlcv_src, _lastdate(ohlcv_dst), _nextdate(ohlcv_dst, _tfr(w))
+    )
+end
+@doc "This function checks that the date candidate is the correct next date to append to `ohclv_dst`."
+function _resolve(w, ohlcv_dst, date_candidate::DateTime, sym=_sym(w))
+    tf = _tfr(w)
+    left = _lastdate(ohlcv_dst)
+    right = date_candidate
+    next = _nextdate(ohlcv_dst, tf)
+    if next < right
+        _fetchto!(w, ohlcv_dst, sym, tf; to=right, from=left)
+    else
+        @debug @assert isrightadj(right, left, tf) "Should $(right) is not right adjacent to $(left)!"
+    end
+end
+
+function _append_ohlcv!(w, ohlcv_dst, ohlcv_src, left, next)
+    # at initialization it can happen that processing is too slow
+    # and fetched ohlcv overlap with processed ohlcv
+    @debug @assert _lastdate(ohlcv_dst) == left
+    @debug @assert left + _tfr(w) == next
+    from_range = rangeafter(ohlcv_src.timestamp, left)
+    if length(from_range) > 0 && _firstdate(ohlcv_src) == next
+        @debug "Appending trades from $(_firstdate(ohlcv_src, from_range)) to $(_lastdate(ohlcv_src))"
+        appendmax!(ohlcv_dst, view(ohlcv_src, from_range, :), w.capacity.view)
+        _check_contig(w, w.view)
+    end
 end
