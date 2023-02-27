404e3149c6295a077ce3096980c12069a67763bd
untoreh <francesco.giannelli@yahoo.it>
2023-02-27 21:23:42 +0100

    libs(Watcher): moved generic load/flush to utils

--- Watchers/src/impls/utils.jl
+++ Watchers/src/impls/utils.jl
@@ -1,3 +1,11 @@
+using Data: _contiguous_ts, nrow, save_ohlcv, zi, check_all_flag, sanitize_pair
+using Data.DataFramesMeta
+using Exchanges.Ccxt: _multifunc
+using Fetch: fetch_candles
+using Lang: @ifdebug
+using Misc: rangeafter, rangebetween
+using Processing: cleanup_ohlcv_data, iscomplete, isincomplete
+
 _parsedatez(s::AbstractString) = begin
     s = rstrip(s, 'Z')
     Base.parse(DateTime, s)
@@ -31,12 +39,11 @@ end
 macro append_dict_data(dict, data, maxlen_var)
     maxlen = esc(maxlen_var)
     quote
-        let doappend((key, newdata)) = begin
-                df = @kget! $(esc(dict)) key DataFrame(; copycols=false)
-                appendmax!(df, newdata, $maxlen)
-            end
-            foreach(doappend, $(esc(data)))
+        function doappend((key, newdata))
+            df = @kget! $(esc(dict)) key DataFrame(; copycols=false)
+            appendmax!(df, newdata, $maxlen)
         end
+        foreach(doappend, $(esc(data)))
     end
 end
 
@@ -54,21 +61,30 @@ _do_check_contig(w, df, ::Val{:on}) = _contiguous_ts(df.timestamp, timefloat(_tf
 _do_check_contig(_, _, ::Val{:off}) = nothing
 _check_contig(w, df) = !isempty(df) && _do_check_contig(w, df, _checks(w))
 
+_tfunc!(attrs, suffix) = attrs[:tfunc] = _multifunc(exc, suffix, true)[1]
 _tfunc(w::Watcher) = w.attrs[:tfunc]
 _exc(w::Watcher) = w.attrs[:exc]
+_exc!(attrs, exc) = attrs[:exc] = exc
+_exc!(w::Watcher, exc) = _exc!(w.attrs, exc)
 _sym(w::Watcher) = w.attrs[:sym]
-_tfr(w::Watcher) = w.attrs[:timeframe]
-_trades(w::Watcher) = w.attrs[:trades]
+_sym!(attrs, v) = attrs[:sym] = v
+_sym!(w::Watcher, v) = _sym!(w.attrs, v)
+_tfr(attrs) = attrs[:timeframe]
+_tfr(w::Watcher) = _tfr(w.attrs)
+_tfr!(attrs, tf) = attrs[:timeframe] = tf
+_tfr!(w::Watcher, tf) = w.attrs[:timeframe] = tf
 _firstdate(df::DataFrame, range::UnitRange) = df[range.start, :timestamp]
 _firstdate(df::DataFrame) = df[begin, :timestamp]
 _firsttrade(w::Watcher) = first(_trades(w))
 _lasttrade(w::Watcher) = last(_trades(w))
 _lastdate(df::DataFrame) = df[end, :timestamp]
 _nextdate(df::DataFrame, tf) = df[end, :timestamp] + period(tf)
-_lastdate(z::ZArray) = z[end, 1] # the first col is a timestamp
+_lastdate(z::ZArray) = z[end, 1] # the first col is a to
 _curdate(tf) = apply(tf, now())
 _nextdate(tf) = _curdate(tf) + tf
 _dateidx(tf, from, to) = max(1, (to - from) ÷ period(tf))
+_lastflushed!(w::Watcher, v) = w.attrs[:last_flushed] = v
+_lastflushed(w::Watcher) = w.attrs[:last_flushed]
 
 struct Warmed end
 struct Pending end
@@ -86,7 +102,8 @@ macro ispending(w)
 end
 _warmed!(_, ::Warmed) = nothing
 _warmed!(w, ::Pending) = w.attrs[:status] = Warmed()
-_pending!(w::Watcher) = w.attrs[:status] = Pending()
+_pending!(attrs) = attrs[:status] = Pending()
+_pending!(w::Watcher) = _pending!(w.attrs)
 _status(w::Watcher) = w.attrs[:status]
 _chill!(w) = w.attrs[:next] = apply(_tfr(w), now())
 _warmup!(_, ::Warmed) = nothing
@@ -99,12 +116,52 @@ macro warmup!(w)
     :(_warmup!($w, _status($w)))
 end
 
-_key!(w, k) = w.attrs[:key] = k
+_key!(w::Watcher, k) = w.attrs[:key] = k
 _view!(w, v) = w.attrs[:view] = v
 _view(w) = w.attrs[:view]
 
+function _get_available(w, z, to)
+    max_lookback = to - _tfr(w) * w.capacity.view
+    isempty(z) && return nothing
+    maxlen = min(w.capacity.view, size(z, 1))
+    available = @view(z[(end - maxlen + 1):end, :])
+    return if dt(available[end, 1]) < max_lookback
+        # data is too old, fetch just the latest candles,
+        # and schedule a background task to fast forward saved data
+        nothing
+    else
+        Data.to_ohlcv(available[:, :])
+    end
+end
+
+function _delete_ohlcv!(w, sym=_sym(w))
+    z = load(zi, _exc(w).name, sanitize_pair(sym), string(_tfr(w)); raw=true)[1]
+    delete!(z)
+end
+
+function _fastforward(w, sym=_sym(w))
+    tf = _tfr(w)
+    df = w.view
+    z = load(zi, _exc(w).name, sym, string(tf); raw=true)[1]
+    @debug @assert isempty(z) || _lastdate(z) != 0 "Corrupted storage because last date is 0: $(_lastdate(z))"
+
+    cur_timestamp = _curdate(tf)
+    avl = _get_available(w, z, cur_timestamp)
+    from = if isnothing(avl)
+        (cur_timestamp - period(tf) * w.capacity.view) - period(tf)
+    else
+        appendmax!(df, avl, w.capacity.view)
+        _check_contig(w, df)
+        _lastdate(df)
+    end
+    if from != cur_timestamp
+        _fetchto!(w, w.view, sym, tf; to=cur_timestamp, from)
+        _check_contig(w, df)
+    end
+end
+
 function _fetch_candles(w, from, to="", sym=_sym(w))
-    fetch_candles(_exc(w), _tfr(w), sym; zi=zilmdb(), from, to)
+    fetch_candles(_exc(w), _tfr(w), sym; from, to)
 end
 
 function _fetch_error(w, from, to, sym=_sym(w))
@@ -113,29 +170,28 @@ end
 
 _op(::Val{:append}, args...; kwargs...) = appendmax!(args...; kwargs...)
 _op(::Val{:prepend}, args...; kwargs...) = prependmax!(args...; kwargs...)
-_fromto(to, tf, cap, kept) = to - period(tf) * (cap - kept) - (2period(tf))
+_fromto(to, prd, cap, kept) = to - prd * (cap - kept) - 2prd
 function _from(df, to, tf, cap, ::Val{:append})
     @debug @assert to >= _lastdate(tf)
-    kept = isempty(df) ? 0 : _lastdate(df) - to + period(tf), period(tf)
-    _fromto(to, tf, cap, kept)
+    date_cap = (to - tf * cap) - tf # add one more period to ensure from inclusion
+    (isempty(df) ? date_cap : max(date_cap, _lastdate(df)))
 end
-_from(df, to, tf, cap, ::Val{:prepend}) = _fromto(to, tf, cap, nrow(df))
+_from(df, to, tf, cap, ::Val{:prepend}) = _fromto(to, period(tf), cap, nrow(df))
 
 @doc "`op`: `appendmax!` or `prependmax!`"
 function _fetchto!(w, df, sym, tf, op=Val(:append); to, from=nothing)
     rows = nrow(df)
+    prd = period(tf)
     rows > 0 && _check_contig(w, df)
-    from = @something from _from(df, to, tf, w.capacity.view, op)#  if isempty(df)
-    if (to - from).value > 0
-        candles = _fetch_candles(w, from, to, sym) # add one more period to ensure from inclusion
+    from = @something from _from(df, to, tf, w.capacity.view, op)
+    diff = (to - from)
+    if diff > prd || (diff == prd && to < _curdate(tf)) # the second case would fetch only the last incomplete candle
+        candles = _fetch_candles(w, from, to, sym)
         from_to_range = rangebetween(candles.timestamp, from, to)
         isempty(from_to_range) && _fetch_error(w, from, to, sym)
         @debug begin
             to,
-            candles[end, :timestamp],
-            from,
-            candles[begin, :timestamp],
-            length(from_to_range),
+            _lastdate(candles), from, _firstdate(candles), length(from_to_range),
             nrow(candles)
         end
         sliced = if length(from_to_range) == nrow(candles)
@@ -145,7 +201,7 @@ function _fetchto!(w, df, sym, tf, op=Val(:append); to, from=nothing)
         end
         @debug sliced[begin, :timestamp]
         cleaned = cleanup_ohlcv_data(sliced, tf)
-        _firstdate(cleaned) != from + period(tf) && _fetch_error(w, from, to, sym)
+        _firstdate(cleaned) != from + prd && _fetch_error(w, from, to, sym)
         _op(op, df, cleaned, w.capacity.view)
         @debug @assert nrow(df) <= w.capacity.view
     end
@@ -181,4 +237,20 @@ function _append_ohlcv!(w, ohlcv_dst, ohlcv_src, left, next)
         appendmax!(ohlcv_dst, view(ohlcv_src, from_range, :), w.capacity.view)
         _check_contig(w, w.view)
     end
+end
+
+function _flushfrom!(w)
+    isempty(w.view) && return nothing
+    # we assume that _load! and process already clean the data
+    from_date = max(_firstdate(w.view), _lastflushed(w))
+    from_date == _lastdate(w.view) && return nothing
+    save_ohlcv(
+        zi,
+        _exc(w).name,
+        _sym(w),
+        string(_tfr(w)),
+        w.view[DateRange(from_date)];
+        check=@ifdebug(check_all_flag, :none)
+    )
+    _lastflushed!(w, _lastdate(w.view))
 end
