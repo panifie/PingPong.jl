f6903e7144b1d47e48cafae88a859c44916abdde
untoreh <francesco.giannelli@yahoo.it>
2023-03-08 16:01:37 +0100

    feat(Sim): roi

---
+++ Engine/src/sim/rois.jl
@@ -0,0 +1,90 @@
+using Misc: Iterable
+
+# TYPENUM
+struct Roi5{T,N}
+    targets::NTuple{N,T}
+    timeouts::NTuple{N,Period}
+    timeframe::TimeFrame
+    function Roi5(tups; timeframe=tf"1m")
+        values = collect(tups)
+        T = typeof(values[1][1])
+        N = length(values)
+        # round
+        timeouts = [round(v[2], timeframe.period) for v in values]
+        # sorted index
+        sorted_idx = sortperm(timeouts)
+        new{T,N}(
+            tuple((values[i][1] for i in sorted_idx)...),
+            # construct sorted tuple
+            tuple((timeouts[i] for i in sorted_idx)...),
+            timeframe,
+        )
+    end
+    Roi5(targets, timeouts; timeframe=tf"1m") = Roi5(zip(targets, timeouts); timeframe)
+end
+Roi = Roi5
+
+_roigetindex(r, i) = (r.targets[i], r.timeouts[i])
+_roilen(r) = length(r.targets)
+_roilastindex(r) = lastindex(r.targets)
+Base.getindex(r::Roi, i) = _roigetindex(r, i)
+Base.length(r::Roi) = _roilen(r)
+Base.lastindex(r::Roi) = _roilastindex(r)
+
+# TYPENUM
+struct RoiInverted1{N,T}
+    targets::NTuple{N,T}
+    timeouts::NTuple{N,Period}
+    timeframe::TimeFrame
+    function RoiInverted1(roi::Roi{T,N}) where {N,T}
+        new{N,T}(reverse(roi.targets), reverse(roi.timeouts), roi.timeframe)
+    end
+end
+RoiInverted = RoiInverted1
+Base.getindex(r::RoiInverted, i) = _roigetindex(r, i)
+Base.length(r::RoiInverted) = _roilen(r)
+Base.lastindex(r::RoiInverted) = _roilastindex(r)
+
+""" roi calculation weighted by elapsed time (number of timeframes), weight is linear """
+function roiweight(elapsed, target, timeout, next_target, next_timeout)
+    timespan = next_timeout - timeout
+    next_ratio = timespan > Second(0) ? (elapsed - timeout) / timespan : 1
+    ratio = 1 - max(0, next_ratio)
+    return target * ratio + next_target * next_ratio
+end
+function roiweight(timeframe::TimeFrame, frames, args...)
+    roiweight(timeframe * frames, args...)
+end
+
+"""
+Checks if roi has been triggered according to a target profit and a *sorted* vector (profit, timeout) tuples.
+Example roi: [(0.1, 3), (0.05, 5), (0.025, 10)]
+In this case 3,5,10 are the timeouts, expressed in timeframes counts. E.g. if the timeframe is 2m then the timeouts
+would effectively be 6m,10m,20m.
+The profit targets 0.1,0.05,0.025 are instead expressed in percentage of the trade value.
+Note how _usually_ with smaller timeouts you expect higher profits, and as time progresses, you settle with lower
+ones, (or even negative ones, which would act as a stoploss.)
+"""
+function isroi(frames, target, roii::RoiInverted)
+    elapsed = frames * roii.timeframe.period
+    # checking for roi is always done in reverse
+    # otherwise the first one would always trigger
+    for (t, tm) in Iterators.drop(enumerate(roii.timeouts), 1)
+        if tm <= elapsed
+            next_t = t > 1 ? t - 1 : t
+            weighted_target = roiweight(
+                elapsed,
+                # current roi target target
+                roii.targets[t],
+                # current roi timeout
+                tm,
+                # next roi value
+                roii.targets[next_t],
+                # next roi timeout
+                roii.timeouts[next_t],
+            )
+            target >= weighted_target && return (true, weighted_target)
+        end
+    end
+    return (false, NaN)
+end
---
+++ test/test_roi.jl
@@ -0,0 +1,48 @@
+using Test
+
+_roi_1() = begin
+    roi = sim.Roi([(0.1, Second(130)), (0.05, Hour(1)), (0.01, Minute(30))])
+    @test roi[1] == (0.1, Minute(2))
+    @test roi[end] == (0.05, Hour(1))
+    @test issorted(roi.timeouts)
+    roii = sim.RoiInverted(roi)
+    @test reverse(roii.timeouts) == roi.timeouts
+    @test roii[1] == (0.05, Hour(1))
+end
+
+_roi_2() = begin
+    target = 0.1
+    next_target = 0.05
+    timeout = Minute(10)
+    next_timeout = Minute(45)
+    w_1 = sim.roiweight(Minute(0), target, timeout, next_target, next_timeout)
+    @test next_target <= w_1 <= target
+    w_2 = sim.roiweight(Minute(45), target, timeout, next_target, next_timeout)
+    @test next_target <= w_2 <= target
+    @test next_target < target && w_2 <= w_1
+    w_3 = sim.roiweight(Minute(100), target, timeout, next_target, next_timeout)
+    @test w_3 <= 0
+end
+
+_roi_3() = begin
+    roi = sim.Roi([(0.1, Minute(30)), (0.03, Hour(1)), (0, Day(1))], timeframe=tf"5m")
+    roii = sim.RoiInverted(roi)
+    t = sim.isroi(2, 0.3, roii)
+    @test !t[1] && isnan(t[2])
+    t = sim.isroi(10, 0.3, roii)
+    @test t[1] && isapprox(t[2], 0.0534, atol=1e-4)
+    t = sim.isroi(10, -0.5, roii)
+    @test !t[1] && isnan(t[2])
+    t = sim.isroi(20 * 24, 0, roii)
+    @test t[1] && t[2] < 0
+end
+
+test_roi() = @testset "roi" begin
+    @eval begin
+        using TimeTicks
+        using PingPong.Engine.Sim: Sim as sim
+        using Data: Data as da
+    end
+    _roi_1()
+    _roi_2()
+end
