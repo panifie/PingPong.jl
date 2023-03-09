6df486589a0b7a779770847b846553877295a704
untoreh <francesco.giannelli@yahoo.it>
2023-03-09 13:46:20 +0100

    lib(Processing): decomposed resample

--- Analysis/Processing/src/resample.jl
+++ Analysis/Processing/src/resample.jl
@@ -1,46 +1,51 @@
-using Pbar
+using TimeTicks
 using ExchangeTypes: Exchange
 using Lang: passkwargs, @ifdebug
-using TimeTicks
-using Data: data_td, save_ohlcv, PairData, empty_ohlcv
+using Data: data_td, save_ohlcv, PairData, empty_ohlcv, DataFrames
 using Data.DFUtils
-using DataFrames
+using .DataFrames
+using Pbar
 
-function resample(exc::Exchange, pair::PairData, to_tf; kwargs...)
-    begin
-        resample(exc, pair.name, pair.data, pair.tf, to_tf; kwargs...)
+# remove incomplete candles at timeseries edges, a full resample requires candles with range 1:frame_size
+function _left_and_right(data, frame_size, src_td, td)
+    left = 1
+    while (timefloat(data.timestamp[left])) % td != 0.0
+        left += 1
     end
+    right = size(data, 1)
+    let last_sample_candle_remainder = src_td * (frame_size - 1)
+        while (timefloat(data.timestamp[right])) % td != last_sample_candle_remainder
+            right -= 1
+        end
+    end
+    left, right
+end
+
+function _deltas(data, to_tf)
+    # NOTE: need at least 2 points
+    result(f=NaN, s=NaN, t=NaN; abort=nothing) = (f, s, t, abort)
+    sz = size(data, 1)
+    sz > 1 || return result(; abort=empty_ohlcv())
+
+    td = timefloat(to_tf)
+    src_prd = timeframe(data).period
+    src_td = timefloat(src_prd)
+
+    @assert td >= src_td "Upsampling not supported. (from $((td_tf[src_td])) to $(td_tf[td]))"
+    td === src_td && return result(; abort=data)
+    frame_size::Integer = td ÷ src_td
+    sz >= frame_size || return result(; abort=empty_ohlcv())
+    result(frame_size, src_td, td)
 end
 
 @doc "Resamples ohlcv data from a smaller to a higher timeframe."
 function resample(exc::Exchange, pairname, data, from_tf, to_tf; save=false)
     @ifdebug @assert all(cleanup_ohlcv_data(data, from_tf).timestamp .== data.timestamp) \
         "Resampling assumptions are not met, expecting cleaned data."
-    # NOTE: need at least 2 points
-    sz = size(data, 1)
-    sz > 1 || return empty_ohlcv()
 
-    td = timefloat(to_tf)
-    # src_prd = data_td(data)
-    src_prd = timeframe(data).period
-    src_td = timefloat(src_prd)
-
-    @assert td >= src_td "Upsampling not supported. (from $((td_tf[src_td])) to $(td_tf[td]))"
-    td === src_td && return data
-    frame_size::Integer = td ÷ src_td
-    sz >= frame_size || return empty_ohlcv()
-
-    # remove incomplete candles at timeseries edges, a full resample requires candles with range 1:frame_size
-    left = 1
-    while (timefloat(data.timestamp[left])) % td !== 0.0
-        left += 1
-    end
-    right = size(data, 1)
-    let last_sample_candle_remainder = src_td * (frame_size - 1)
-        while (timefloat(data.timestamp[right])) % td !== last_sample_candle_remainder
-            right -= 1
-        end
-    end
+    frame_size, src_td, td, abort = _deltas(data, to_tf)
+    isnothing(abort) || return abort
+    left, right = _left_and_right(data, frame_size, src_td, td)
 
     # Create a new dataframe to keep thread safety
     data = DataFrame(@view(data[left:right, :]); copycols=false)
@@ -61,8 +66,12 @@ function resample(exc::Exchange, pairname, data, from_tf, to_tf; save=false)
     select!(data, Not(:sample))
     select!(df, Not(:sample))
     save && size(df)[1] > 0 && save_ohlcv(exc, pairname, string(to_tf), df)
-    @debug @show "last 2 candles: " df[end - 1, :timestamp] df[end, :timestamp]
+    @debug "last 2 candles: " df[end - 1, :timestamp] df[end, :timestamp]
     df
+end
+
+function resample(exc::Exchange, pair::PairData, to_tf; kwargs...)
+    resample(exc, pair.name, pair.data, pair.tf, to_tf; kwargs...)
 end
 
 function resample(
