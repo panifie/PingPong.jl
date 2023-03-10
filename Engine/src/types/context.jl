f21251c2d710b799d37572afe5a0856ba5769746
untoreh <francesco.giannelli@yahoo.it>
2023-03-10 15:38:23 +0100

    fix: Ctx construction

--- Engine/src/types/context.jl
+++ Engine/src/types/context.jl
@@ -1,37 +1,38 @@
 using Misc
 using TimeTicks
 
+# TYPENUM
 @doc """The configuration against which a strategy is tested.
 - `timeframe`: The time step or "tick" that the backtesting iteration uses.
 - `spread`: affects the weight of the spread calculation (based on ohlcv)."
 - `slippage`: affects the weight of the spread calculation (based on volume and trade size).
 """
-struct Context8
+struct Context9
     range::DateRange
     spread::Float64
     slippage::Float64
-    function Context8(tf, from_date, to_date; spread=1.0, slippage=1.0)
-        begin
-            current_date = [apply(tf, from_date)]
-            new(tf, from_date, to_date, current_date, spread, slippage)
-        end
+    Context9(d::DateRange; spread=1.0, slippage=1.0) = begin
+        new(d, spread, slippage)
     end
-    function Context8(
-        timeframe::T, from_date::T, to_date::T; spread=1.0, slippage=1.0
+    function Context9(tf, from_date, to_date; kwargs...)
+        new(DateRange(from_date, to_date, tf), kwargs...)
+    end
+    function Context9(
+        timeframe::T, from_date::T, to_date::T; kwargs...
     ) where {T<:AbstractString}
-        begin
-            from = convert(DateTime, from_date)
-            to = convert(DateTime, to_date)
-            tf = convert(TimeFrame, timeframe)
-            new(tf, from, to, spread, slippage)
-        end
+        from = convert(DateTime, from_date)
+        to = convert(DateTime, to_date)
+        tf = convert(TimeFrame, timeframe)
+        new(DateRange(tf, from, to), kwargs...)
     end
-    Context8(tf::TimeFrame, since::Period) = begin
+    Context9(tf::TimeFrame, since::Period) = begin
         to = now()
         from = to - since
-        Context8(tf, from, to)
+        Context9(DateRange(from, to, tf))
+    end
+    Context9(tf::TimeFrame, since::Integer) = begin
+        from = abs(since) * tf.period
+        Context9(tf, from)
     end
 end
-Context = Context8
-
-export next
+Context = Context9
