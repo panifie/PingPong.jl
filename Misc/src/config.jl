d1afc844467ed2f07f85aa427644cd3b0c78b724
untoreh <francesco.giannelli@yahoo.it>
2023-03-14 15:35:24 +0100

    tweak: decomposed `loadconfig!`

--- Misc/src/config.jl
+++ Misc/src/config.jl
@@ -1,9 +1,7 @@
-using Base: @kwdef
-using Dates: Period
+using Pkg: Pkg
 using TOML
 using JSON
 using TimeTicks
-using Pkg: Pkg
 using FunctionalCollections: PersistentHashMap
 # TODO: move config to own pkg
 
@@ -56,13 +54,8 @@ end
 - `attrs`: Generic metadata container.
 - `sources`: mapping of modules symbols name to (.jl) file paths
 """
-@kwdef mutable struct Config12
+@kwdef mutable struct Config14
     path::String = ""
-    window::Period = Day(7)
-    timeframe::TimeFrame = TimeFrame(Day(1))
-    timeframes::Vector{TimeFrame} = [
-        convert(TimeFrame, t) for t in ("1m", "15m", "1h", "1d")
-    ]
     exchange::Symbol = Symbol()
     qc::Symbol = :USDT
     margin::Bool = false
@@ -71,6 +64,9 @@ end
     vol_min::Float64 = 10e4
     initial_cash::Float64 = 100.0
     base_amount::Float64 = 10.0
+    base_timeframe::TimeFrame = tf"1m"
+    timeframes::Tuple{TimeFrame} = timeframe.(["1m", "15m", "1h", "1d"])
+    window::Period = Day(7) # deprecated
     # - `slope/min/max`: Used in Analysios/slope.
     # - `ct`: Used in Analysis/corr.
     # slope_min::Float64= 0.
@@ -80,52 +76,68 @@ end
     attrs::Dict{Any,Any} = Dict()
     toml = nothing
 end
-Config = Config12
+Config = Config14
 
 @doc "Global configuration instance."
 const config = Config()
 const SourcesDict = Dict{Symbol,String}
 
-@doc "Parses the toml file and populates the global `config`."
-function loadconfig!(
-    name::T; path::String=config_path(), cfg::Config=config
-) where {T<:Union{Symbol,String}}
-    name = convert(Symbol, name)
+_path!(cfg, path) = begin
     if !isfile(path)
         throw("Config file not found at path $(config.path)")
     else
         cfg.path = path
     end
-    name = string(name)
+end
+
+_namestring(name) = begin
+    name = convert(Symbol, name)
+    string(name)
+end
+
+function _toml!(cfg, name)
     cfg.toml = PersistentHashMap(k => v for (k, v) in TOML.parsefile(cfg.path))
     if name ∉ keys(cfg.toml)
         throw("Config section [$name] not found in the configuration read from $(cfg.path)")
     end
-    kwargs = Dict{Symbol,Any}()
+end
+function _options!(cfg, name)
     options = fieldnames(Config)
     for (opt, val) in cfg.toml[name]
         sym = Symbol(opt)
         if sym ∈ options
-            kwargs[sym] = val
+            setcfg!(cfg, sym, val)
         else
             cfg.attrs[opt] = val
         end
-        # setcfg!(Symbol(opt), val)
     end
+end
+_sources!(cfg, name) = begin
     for (k, v) in cfg.toml["sources"]
         cfg.sources[Symbol(k)] = v
     end
     for k in setdiff(keys(cfg.toml), Set([name, "sources"]))
         cfg.attrs[k] = cfg.toml[k]
     end
+end
+
+@doc "Parses the toml file and populates the global `config`."
+function loadconfig!(
+    profile::T; path::String=config_path(), cfg::Config=config
+) where {T<:Union{Symbol,String}}
+    _path!(cfg, path)
+    name = _namestring(profile)
+    _toml!(cfg, name)
+    _options!(cfg, name)
+    _sources!(cfg, name)
     cfg
 end
 
 @doc "Reset global config to default values."
-function resetconfig!()
+function resetconfig!(c=config)
     default = Config()
     for k in fieldnames(Config)
-        setcfg!(k, getproperty(default, k))
+        setcfg!(c, k, getproperty(default, k))
     end
 end
 
@@ -140,7 +152,8 @@ macro lev!()
 end
 
 @doc "Sets a single config value."
-setcfg!(k, v) = setproperty!(config, k, v)
+setcfg!(c::Config, k, v) = setproperty!(c, k, v)
+globalcfg!(k, v) = setcfg!(config, k, v)
 
 resetconfig!()
 
