cb75851cf94b0b50b37b134c5cc9b9c6c0536f7d
untoreh <francesco.giannelli@yahoo.it>
2023-03-17 17:05:43 +0100

    ref: add function to cleanup compile cache

--- resolve.jl
+++ resolve.jl
@@ -1,27 +1,62 @@
 using Pkg: Pkg
 
-@doc "Recursively resolves all julia projects in a directory."
-function recurse_projects(path="."; top=true, doupdate=false, precomp=false)
+function recurse_projects(
+    f, path="."; top=true, exclude=("test", "docs", "deps"), kwargs...
+)
     path = realpath(path)
     for subpath in readdir(path)
         fullpath = joinpath(path, subpath)
         if endswith(fullpath, "Project.toml")
-            projpath = dirname(joinpath(path, fullpath))
-            Pkg.activate(projpath)
-            Pkg.resolve()
-            doupdate && begin
-                Pkg.offline(false)
-                Pkg.update()
-                Pkg.offline(true)
-            end
-            precomp && begin
-                Pkg.precompile()
-            end
+            f(path, fullpath; kwargs...)
         elseif isdir(fullpath)
-            if !startswith(fullpath, ".") && !endswith(fullpath, "test")
-                recurse_projects(fullpath; top=false)
+            if !startswith(fullpath, ".") && all((!endswith(fullpath, e) for e in exclude))
+                recurse_projects(f, fullpath; top=false, kwargs...)
             end
         end
     end
     top && Pkg.activate(pwd())
+end
+
+function _update_project(path, fullpath; precomp, doupdate)
+    projpath = dirname(joinpath(path, fullpath))
+    Pkg.activate(projpath)
+    Pkg.resolve()
+    doupdate && begin
+        Pkg.offline(false)
+        Pkg.update()
+        Pkg.offline(true)
+    end
+    precomp && begin
+        Pkg.precompile()
+    end
+end
+
+function update_projects(path="."; doupdate=false, precomp=false)
+    recurse_projects(_update_project, path; doupdate, precomp)
+end
+
+function _project_name!(path, fullpath; projects)
+    projpath = dirname(joinpath(path, fullpath))
+    Pkg.activate(projpath)
+    name = Pkg.project().name
+    isnothing(name) || push!(projects, name)
+end
+
+function projects_name(path=".")
+    projects = Set{String}()
+    recurse_projects(_project_name!, path; projects)
+    projects
+end
+
+function purge_compilecache(path=".")
+    names = projects_name(path)
+    compiled = joinpath(
+        homedir(), ".julia", "compiled", "v$(VERSION.major).$(VERSION.minor)"
+    )
+    for name in names
+        pkg_path = joinpath(compiled, name)
+        if isdir(pkg_path)
+            rm(pkg_path; recursive=true)
+        end
+    end
 end
