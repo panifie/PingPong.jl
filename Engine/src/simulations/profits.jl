0b860513542465b951cc8b7fe147ef694594c50e
untoreh <francesco.giannelli@yahoo.it>
2023-03-17 17:33:45 +0100

    tweak: more meaningful var names

--- Engine/src/simulations/profits.jl
+++ Engine/src/simulations/profits.jl
@@ -6,17 +6,16 @@ ishighfirst(open, close) = close <= open
 - The `*_price` is the _quote_ price, usually within OHLC boundaries.
 "
 function profitat(open_price::Real, close_price, fee; digits=8)
-    shares = 1. / open_price
+    shares = 1.0 / open_price
     # How much the trade is worth at open
-    open_value = shares * open_price
+    size_open = shares * open_price
     # How much the trade is worth at close
-    close_value = shares * close_price
+    size_close = shares * close_price
     # How much was spent opening the trade with fees
-    value_spent = open_value + open_value * fee
+    cost = size_open + size_open * fee
     # How much was returned, minus fees
-    value_returned = close_value - close_value * fee
-    profits = value_returned / value_spent - 1.0
+    cash = size_close - size_close * fee
+    profits = cash / cost - 1.0
     round(profits; digits)
 end
 
-slippage(args...) = 0.0
