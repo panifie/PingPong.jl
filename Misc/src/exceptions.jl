6e16c106b4cf92c492c8075fccfc70fc51ef667a
untoreh <francesco.giannelli@yahoo.it>
2023-03-22 14:41:31 +0100

    fix:

--- Misc/src/exceptions.jl
+++ Misc/src/exceptions.jl
@@ -8,17 +8,16 @@ struct RightContiguityException <: ContiguityException
     new_date::AbstractDateTime
 end
 
-function display(e::RightContiguityException)
-    "Data stored ends at $(e.stored_date) while new data starts at $(e.new_date)."
+function show(io::IO, e::RightContiguityException)
+    write(io, "Data stored ends at $(e.stored_date) while new data starts at $(e.new_date).")
 end
-show(e::RightContiguityException) = display(e)
 
 struct LeftContiguityException <: Exception
     stored_date::AbstractDateTime
     new_date::AbstractDateTime
 end
 
-function display(e::LeftContiguityException)
-    "Data stored starts at $(e.stored_date) while new data ends at $(e.new_date)."
+function show(io::IO, e::LeftContiguityException)
+    write(io, "Data stored starts at $(e.stored_date) while new data ends at $(e.new_date).")
 end
 show(e::LeftContiguityException) = display(e)
