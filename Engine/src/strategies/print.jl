60a6a1838e09bbd47edee599da764dfb0120009b
untoreh <francesco.giannelli@yahoo.it>
2023-03-22 14:43:08 +0100

    fix: strategy print func

--- Engine/src/strategies/print.jl
+++ Engine/src/strategies/print.jl
@@ -1,6 +1,5 @@
 minmax_holdings(s::Strategy) = begin
     n_holdings = 0
-    n_trades = 0
     max_hold = 0.0
     min_hold = Inf
     for ai in s.holdings
@@ -11,20 +10,34 @@ minmax_holdings(s::Strategy) = begin
         if 0 < ai.cash.value < min_hold
             min_hold = ai.cash
         end
+    end
+    (min=min_hold, max=max_hold, count=n_holdings)
+end
+
+trades_total(s::Strategy) = begin
+    n_trades = 0
+    for ai in s.universe.data.instance
         n_trades += length(ai.history)
     end
-    (min=min_hold, max=max_hold, count=n_holdings, trades=n_trades)
+    n_trades
 end
 
 function Base.show(out::IO, s::Strategy)
     write(out, "Name: $(nameof(s))\n")
-    write(out, "Config: $(s.config.min_amount)(Base Amount), $(s.config.initial_cash)(Initial Cash)\n")
+    write(
+        out,
+        "Config: $(s.config.min_size)(Base Size), $(s.config.initial_cash)(Initial Cash)\n",
+    )
     n_inst = nrow(s.universe.data)
     n_exc = length(unique(s.universe.data.exchange))
     write(out, "Universe: $n_inst instances, $n_exc exchanges")
     write(out, "\n")
     mmh = minmax_holdings(s)
-    write(out, "Holdings: assets(trades): $(mmh.count)($(mmh.trades)), min $(mmh.min), max $(mmh.max)\n")
+    n_trades = trades_total(s)
+    write(
+        out,
+        "Holdings: assets(trades): $(mmh.count)($(n_trades)), min $(mmh.min), max $(mmh.max)\n",
+    )
     write(out, "Orders: $(length(s.orders))\n")
     write(out, "$(s.cash) (Cash)\n")
     write(out, "$(current_worth(s)) (Worth)")
