6626ab63a45cb1580540e6d5247b20393464b6c4
panbonker <francesco.giannelli@yahoo.it>
2023-04-20 15:28:58 +0200

    lib(PingPong): initial precompiles for backtesting

--- PingPong/src/PingPong.jl
+++ PingPong/src/PingPong.jl
@@ -9,6 +9,7 @@ else
     occursin(string(@__MODULE__), get(ENV, "JULIA_NOPRECOMP", "")) && __precompile__(false)
     include("pingpong.jl")
     __init__() = _doinit()
+    include("precompile.jl")
 end
 
 end # module
--- PingPong/src/precompile.jl
+++ PingPong/src/precompile.jl
@@ -1,12 +1,11 @@
-using DataFrames: DataFrame
+using Lang: @preset, @precomp
 
-ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
 
-precompile(@in_repl, ())
-precompile(PingPong.Data.load, (AbstractString,))
-precompile(PingPong.Data.load, (ZarrInstance, AbstractString, String, String))
-precompile(PingPong.Plotting.plotgrid, (DataFrame, Int, AbstractVector{String}))
-
-precompile(Tuple{typeof(PingPong.Analysis.__init__)})
-precompile(Tuple{typeof(PingPong.Data.__init__)})
-precompile(Tuple{typeof(PingPong.Misc.Pbar.__init__)})
+@preset let
+    using Stubs
+    @precomp let
+        s = Stubs.stub_strategy()
+        Engine.SimMode.backtest!(s)
+        ai = first(s.universe)
+    end
+end
