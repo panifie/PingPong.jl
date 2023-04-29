e6b3ffeb9c313ec2bb48dd04bc0c6d0a588672db
panbonker <francesco.giannelli@yahoo.it>
2023-04-29 15:23:17 +0200

    tests: backtest

--- PingPong/test/test_backtest.jl
+++ PingPong/test/test_backtest.jl
@@ -1,4 +1,26 @@
+using Test
 
-include("env.jl")
+_eq4(a, b) = isapprox(a, b; atol=1e-5)
+_test_nomargin() = begin
+    s = egn.strategy(:Example)
+    Random.seed!(1)
+    Stubs.stub!(s)
+    egn.backtest!(s)
+    @test marginmode(s) == NoMargin
+    @test _eq4(Cash(:USDT, 9.8795), s.cash.value)
+    @test s.cash_committed == Cash(:USDT, 0.0)
+    @test st.trades_total(s) == 2951
+    mmh = st.minmax_holdings(s)
+    @test mmh.count == 0
+    @test mmh.min == (:USDT, Inf)
+    @test mmh.max == (:USDT, 0.0)
+end
 
-test_backtest() = @testset "backtest" begin end
+test_backtest() = @testset "backtest" begin
+    @eval include(joinpath(@__DIR__, "env.jl"))
+    @eval begin
+        using Stubs
+        using Random
+    end
+    @testset _test_nomargin()
+end
