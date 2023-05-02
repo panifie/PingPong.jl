8f8d9d879e311429231eb5712b0b3f101336d286
panbonker <francesco.giannelli@yahoo.it>
2023-05-02 19:46:50 +0200

    test: update market test
    because synth function changed

--- PingPong/test/test_backtest.jl
+++ PingPong/test/test_backtest.jl
@@ -1,37 +1,52 @@
+using Stubs
 using Test
+using Random
+using Lang: @m_str
 
 _openval(s, a) = s.universe[a].instance.ohlcv.open[begin]
 _closeval(s, a) = s.universe[a].instance.ohlcv.close[end]
-_eq4(a, b) = isapprox(a, b; atol=1e-4)
-_test_nomargin() = begin
-    s = egn.strategy(:Example)
-    Random.seed!(1)
-    Stubs.stub!(s)
-    egn.backtest!(s)
+_test_synth(s) = begin
     @test _openval(s, m"sol") == 101.0
     @test _closeval(s, m"sol") == 1754.0
     @test _openval(s, m"btc") == 99.0
     @test _closeval(s, m"btc") == 580.0
     @test _openval(s, m"eth") == 97.0
     @test _closeval(s, m"eth") == 129.0
+end
+
+_backtest_strat(sym) = begin
+    s = egn.strategy(sym)
+    Random.seed!(1)
+    Stubs.stub!(s)
+    s
+end
+
+_eq4(a, b) = isapprox(a, b; atol=1e-4)
+_test_nomargin_market(s) = begin
+    s.attrs[:ordertype] = :market
+    egn.backtest!(s)
     @test marginmode(s) == NoMargin
-    @test _eq4(Cash(:USDT, 9.7730), s.cash.value)
+    @test _eq4(Cash(:USDT, 9.5524), s.cash.value)
     @test s.cash_committed == Cash(:USDT, 0.0)
-    @test st.trades_total(s) == 2938
+    @test st.trades_total(s) == 3658
     mmh = st.minmax_holdings(s)
     @test mmh.count == 1
-    @test mmh.min[1] == :SOL
-    @test mmh.min[2] ≈ 1.0694938310898965
-    @test mmh.max[1] == :SOL
-    @test mmh.max[2] ≈ 1.0694938310898965
+    @test mmh.min[1] == :BTC
+    @test mmh.min[2] ≈ 8.236349534060919
+    @test mmh.max[1] == :BTC
+    @test mmh.max[2] ≈ 8.236349534060919
+end
+
+_test_nomargin_gtc(s) = begin
+    s.attrs[:ordertype] = :gtc
+    egn.backtest!(s)
+    s
 end
 
 test_backtest() = @testset "backtest" begin
     @eval include(joinpath(@__DIR__, "env.jl"))
-    @eval begin
-        using Stubs
-        using Random
-        using Lang: @m_str
-    end
-    @testset _test_nomargin()
+    s = _backtest_strat(:Example)
+    # @testset _test_synth(s)
+    # @testset _test_nomargin_market(s)
+    @testset _test_nomargin_gtc(s)
 end
