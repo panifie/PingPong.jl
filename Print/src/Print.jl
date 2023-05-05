5952a18f92ede7300501bfe4f9fa3658471538aa
panbonker <francesco.giannelli@yahoo.it>
2023-05-05 14:49:56 +0200

    ref: remove empty proj

--- Print/src/Print.jl
+++
@@ -1,9 +0,0 @@
-module Print
-using Formatting: format
-
-@doc "Print a (currency) number."
-function printn(n, cur="USDT"; precision=2, commas=true, kwargs...)
-    println(format(n; precision, commas, kwargs...), " ", cur)
-end
-
-end # module Print
