0d57e9622234fbd62e8052f0b40393934f770291
panbonker <francesco.giannelli@yahoo.it>
2023-05-14 16:41:18 +0200

    fix: wrong order creation
    orders creation should fail if the opposite position is still open
    in non hedged instances

--- SimMode/src/positions/pong.jl
+++ SimMode/src/positions/pong.jl
@@ -11,7 +11,8 @@ const _PROTECTIONS_WARNING = """
 """
 
 @doc "Creates a simulated limit order, updating a levarged position."
-function pong!(s::IsolatedStrategy{Sim}, ai, t::Type{<:LimitOrder}; amount, kwargs...)
+function pong!(s::IsolatedStrategy{Sim}, ai::MarginInstance, t::Type{<:LimitOrder}; amount, kwargs...)
+    isopen(ai, opposite(orderpos(t))) && return nothing
     o = _create_sim_limit_order(s, t, ai; amount, kwargs...)
     return if !isnothing(o)
         t = limitorder_ifprice!(s, o, o.date, ai)
@@ -34,6 +35,7 @@ function pong!(
     date,
     kwargs...,
 )
+    isopen(ai, opposite(orderpos(t))) && return nothing
     o = _create_sim_market_order(s, t, ai; amount, date, kwargs...)
     isnothing(o) && return nothing
     t = marketorder!(s, o, ai, amount; date)
