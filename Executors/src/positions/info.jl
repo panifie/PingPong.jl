961693701a8d138557329c7023a2f024487304ca
panbonker <francesco.giannelli@yahoo.it>
2023-05-14 16:42:48 +0200

    lib: calc total loss when showing liquidations

--- Executors/src/positions/info.jl
+++ Executors/src/positions/info.jl
@@ -3,15 +3,21 @@ using OrderTypes: LiquidationTrade
 @doc "The number of liquidations that have happened for an asset instance."
 function liquidations(ai::MarginInstance)
     short_liq = Trade[]
+    short_loss = 0.0
     long_liq = Trade[]
+    long_loss = 0.0
     for t in ai.history
         if t isa LiquidationTrade
             if orderpos(t) == Long
+                @deassert t.value - t.fees == t.size
+                long_loss += abs(t.value / t.leverage)
                 push!(long_liq, t)
             else
+                @deassert t.value - t.fees == t.size
+                short_loss += abs(t.value / t.leverage)
                 push!(short_liq, t)
             end
         end
     end
-    (long=long_liq, short=short_liq)
+    (;long=long_liq, long_loss, short=short_liq, short_loss)
 end
