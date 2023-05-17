7afc3bd95ca7961b6fbdb0ac1c7001d3229a10d3
panbonker <francesco.giannelli@yahoo.it>
2023-05-17 19:15:24 +0200

    lib(Exchanges): improve CurrencyCash

--- Exchanges/src/currency.jl
+++ Exchanges/src/currency.jl
@@ -59,63 +59,72 @@ function CurrencyCash{C,E}(v) where {C<:Cash,E<:ExchangeID}
     CurrencyCash(E, C, v)
 end
 
+function CurrencyCash(::CurrencyCash{C,E}, v) where {C<:Cash,E<:ExchangeID}
+    CurrencyCash(E, C, v)
+end
+
 value(cc::CurrencyCash) = value(cc.cash)
 Base.getproperty(c::CurrencyCash, s::Symbol) =
     if s == :value
-        getfield(getfield(c, :cash), :value)[]
+        getfield(_cash(c), :value)[]
     elseif s == :id
-        nameof(getfield(c, :cash))
+        nameof(_cash(c))
     else
         getfield(c, s)
     end
 Base.setproperty!(::CurrencyCash, ::Symbol, v) = error("CurrencyCash is private.")
-Base.zero(c::Union{CurrencyCash,Type{CurrencyCash}}) = zero(c.cash)
-Base.iszero(c::CurrencyCash) = isapprox(value(c), zero(c); atol=c.precision)
+Base.zero(c::Union{CurrencyCash,Type{CurrencyCash}}) = zero(_cash(c))
+Base.iszero(c::CurrencyCash) = isapprox(value(c), zero(c); atol=_prec(c))
 
 function Base.show(io::IO, c::CurrencyCash{<:Cash,E}) where {E<:ExchangeID}
     write(io, "$(c.cash) (on $(E.parameters[1]))")
 end
-Base.hash(c::CurrencyCash, args...) = hash(c.cash, args...)
-Base.nameof(c::CurrencyCash) = nameof(c.cash)
+Base.hash(c::CurrencyCash, args...) = hash(_cash(c)args...)
+Base.nameof(c::CurrencyCash) = nameof(_cash(c))
 
-Base.promote(c::CurrencyCash, n) = promote(c.cash, n)
-Base.promote(n, c::CurrencyCash) = promote(n, c.cash)
+Base.promote(c::CurrencyCash, n) = promote(_cash(c)n)
+Base.promote(n, c::CurrencyCash) = promote(n, _cash(c))
 
-Base.convert(::Type{T}, c::CurrencyCash) where {T<:Real} = convert(T, c.cash)
-Base.isless(a::CurrencyCash, b::CurrencyCash) = isless(a.cash, b.cash)
+Base.convert(::Type{T}, c::CurrencyCash) where {T<:Real} = convert(T, _cash(c))
+Base.isless(a::CurrencyCash, b::CurrencyCash) = isless(_cash(a), _cash(b))
 Base.isless(a::CurrencyCash, b::Number) = isless(promote(a, b)...)
 Base.isless(b::Number, a::CurrencyCash) = isless(promote(b, a)...)
 
-Base.abs(c::CurrencyCash) = abs(c.cash)
-Base.real(c::CurrencyCash) = real(c.cash)
+Base.abs(c::CurrencyCash) = abs(_cash(c))
+Base.real(c::CurrencyCash) = real(_cash(c))
 
-_prec(cc, v) = toprecision(v, cc.precision)
+_cash(cc::CurrencyCash) = getfield(cc, :cash)
+_prec(cc::CurrencyCash) = getfield(cc, :precision)
+_toprec(cc::AbstractCash, v) = toprecision(v, _prec(cc))
+_toprec(cc::AbstractCash, c::Cash) = toprecision(value(c), _prec(cc))
 
--(a::CurrencyCash, b::Real) = _prec(a, a.cash - b)
-+(a::CurrencyCash, b::Real) = _prec(a, a.cash + b)
-*(a::CurrencyCash, b::Real) = _prec(a, a.cash * b)
-/(a::CurrencyCash, b::Real) = _prec(a, a.cash / b)
+-(a::CurrencyCash, b::Real) = _toprec(a, a.cash - b)
++(a::CurrencyCash, b::Real) = _toprec(a, a.cash + b)
+*(a::CurrencyCash, b::Real) = _toprec(a, a.cash * b)
+/(a::CurrencyCash, b::Real) = _toprec(a, a.cash / b)
 ÷(a::CurrencyCash, b::Real) = a.cash ÷ b
 
-==(a::CurrencyCash{S}, b::CurrencyCash{S}) where {S} = b.cash == a.cash
-÷(a::CurrencyCash{S}, b::CurrencyCash{S}) where {S} = a.cash ÷ b.cash
-*(a::CurrencyCash{S}, b::CurrencyCash{S}) where {S} = _prec(a, a.cash * b.cash)
-/(a::CurrencyCash{S}, b::CurrencyCash{S}) where {S} = _prec(a, a.cash / b.cash)
-+(a::CurrencyCash{S}, b::CurrencyCash{S}) where {S} = _prec(a, a.cash + b.cash)
--(a::CurrencyCash{S}, b::CurrencyCash{S}) where {S} = _prec(a, a.cash - b.cash)
+==(a::CurrencyCash{S}, b::CurrencyCash{S}) where {S} = _cash(b) == _cash(a)
+÷(a::CurrencyCash{S}, b::CurrencyCash{S}) where {S} = _cash(a) ÷ _cash(b)
+*(a::CurrencyCash{S}, b::CurrencyCash{S}) where {S} = _toprec(a, _cash(a) * _cash(b))
+/(a::CurrencyCash{S}, b::CurrencyCash{S}) where {S} = _toprec(a, _cash(a) / _cash(b))
++(a::CurrencyCash{S}, b::CurrencyCash{S}) where {S} = _toprec(a, _cash(a) + _cash(b))
+-(a::CurrencyCash{S}, b::CurrencyCash{S}) where {S} = _toprec(a, _cash(a) - _cash(b))
 
 _applyop!(op, c, v) =
-    let cv = getfield(c.cash, :value)
-        cv[1] = _prec(c, op(cv[1], v))
+    let cv = getfield(_cash(c), :value)
+        cv[1] = _toprec(c, op(cv[1], v))
     end
 
 add!(c::CurrencyCash, v) = _applyop!(+, c, v)
 sub!(c::CurrencyCash, v) = _applyop!(-, c, v)
 mul!(c::CurrencyCash, v) = _applyop!(*, c, v)
 rdiv!(c::CurrencyCash, v) = _applyop!(/, c, v)
-div!(c::CurrencyCash, v) = div!(c.cash, v)
-mod!(c::CurrencyCash, v) = mod!(c.cash, v)
-cash!(c::CurrencyCash, v) = cash!(c.cash, _prec(c, v))
+div!(c::CurrencyCash, v) = div!(_cash(c), v)
+mod!(c::CurrencyCash, v) = mod!(_cash(c), v)
+cash!(c::CurrencyCash, v) = cash!(_cash(c), _toprec(c, v))
+addzero!(c::CurrencyCash, v) = addzero!(_cash(c), v; atol=_prec(c))
+subzero!(c::CurrencyCash, v) = subzero!(_cash(c), v; atol=_prec(c))
 
 gtxzero(c::CurrencyCash) = gtxzero(value(c); atol=getfield(c, :precision))
 ltxzero(c::CurrencyCash) = ltxzero(value(c); atol=getfield(c, :precision))
