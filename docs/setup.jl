da8a6c9b4efe85cd7a175b054ed503a4e043a6dc
panbonker <francesco.giannelli@yahoo.it>
2023-06-08 17:05:46 +0200

    fix: make docs setup more resilient

--- docs/setup.jl
+++ docs/setup.jl
@@ -1,6 +1,16 @@
-include("noprecomp.jl")
-using Pkg
-Pkg.add(["Documenter", "DocStringExtensions", "Suppressor"]);
-Pkg.instantiate()
-Pkg.precompile()
-Pkg.precompile()
+let tries = 0
+    while tries < 2
+        try
+            include("noprecomp.jl")
+            using Pkg
+            Pkg.add(["Documenter", "DocStringExtensions", "Suppressor"])
+            Pkg.instantiate()
+            Pkg.precompile()
+            Pkg.precompile()
+            break
+        catch e
+            tries += 1
+            Base.showerror(stderr, e)
+        end
+    end
+end
