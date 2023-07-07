f6d3d8c4d2b4823a7de996401d8d5739f5fa8975
panbonker <francesco.giannelli@yahoo.it>
2023-07-07 06:39:03 +0200

    lib(PaperMode): improve logging

--- PaperMode/src/papermode.jl
+++ PaperMode/src/papermode.jl
@@ -1,9 +1,11 @@
 using SimMode
 using Executors
+using Executors: orders, orderscount
 using Executors.OrderTypes
 using Executors.TimeTicks
 using Executors.Instances
 using Executors.Misc
+using Executors.Instruments: compactnum as cnum
 using .Misc.TimeToLive: safettl
 using .Misc.Lang: @lget!, @deassert
 using Executors.Strategies: MarginStrategy, Strategy, Strategies as st, ping!
@@ -17,18 +19,41 @@ using Fetch: pytofloat
 
 const TradesCache = Dict{AssetInstance,CircularBuffer{CcxtTrade}}()
 
-function run!(s::Strategy{Paper}; throttle=Second(5), doreset=true, verbose=true)
+function run!(
+    s::Strategy{Paper}; throttle=Second(5), doreset=false, verbose=true, foreground=true
+)
     doreset && st.reset!(s)
-    verbose && @info "Starting strategy $(nameof(s)) in paper mode! (throttle: $throttle)"
-    infofunc = if verbose
-        () -> @info "Pinging strategy ($(now()))"
+    verbose && @info "Starting strategy $(nameof(s)) in paper mode!
+
+    throttle: $throttle
+    timeframes: $(string(s.timeframe)) (main), $(string(get(s.attrs, :timeframe, nothing))) (optional), $(join(string.(s.config.timeframes), " ")...) (extras)
+    cash: $(s.cash) [$(cnum(st.current_total(s, lastprice)))]
+    assets: $(let str = join(getproperty.(st.assets(s), :raw), ", "); str[begin:min(length(str), displaysize()[2] - 1)] end)
+    margin: $(marginmode(s))
+    "
+    infofunc = if verbose && foreground
+        () -> begin
+            long, short, liq = st.trades_count(s, Val(:positions))
+            cv = cnum(s.cash.value)
+            comm = cnum(s.cash_committed.value)
+            inc = orderscount(s, Val(:increase))
+            red = orderscount(s, Val(:reduce))
+            tot = st.current_total(s, lastprice) |> cnum
+            @info "$(now())($(nameof(s))@$(s.exchange)) $comm/$cv[$tot]($(nameof(s.cash))), orders: $inc/$red(+/-) trades: $long/$short/$liq(L/S/Q)"
+        end
     else
         Returns(nothing)
     end
-    while true
-        infofunc()
-        ping!(s, now(), nothing)
-        sleep(throttle)
+    doping() =
+        while true
+            infofunc()
+            ping!(s, now(), nothing)
+            sleep(throttle)
+        end
+    if foreground
+        doping()
+    else
+        @async doping()
     end
 end
 
