459aced84b695e9e31750054e50f47627a17ae43
panbonker <francesco.giannelli@yahoo.it>
2023-07-18 14:23:17 +0200

    fix(Python): don't use python threads for async
    Because it leads to races from julia side

--- Python/src/async.jl
+++ Python/src/async.jl
@@ -8,9 +8,9 @@ using ThreadPools: @tspawnat
     pyuv::Py = pynew()
     pythreads::Py = pynew()
     pyrunner::Py = pynew()
-    pyrunner_thread::Py = pynew()
     pyloop::Py = pynew()
     pycoro_type::Py = pynew()
+    task::Ref{Task} = Ref{Task}()
 end
 
 const gpa = PythonAsync()
@@ -40,7 +40,15 @@ function _async_init(pa::PythonAsync)
         py_start_loop(pa)
         if pyisnull(gpa.pyaio)
             for f in fieldnames(PythonAsync)
-                pycopy!(getfield(gpa, f), getfield(pa, f))
+                let v = getfield(gpa, f)
+                    if v isa Py
+                        pycopy!(v, getfield(pa, f))
+                    elseif f == :task
+                        isassigned(v) && (pa.task[] = v[])
+                    else
+                        error()
+                    end
+                end
             end
         end
     end
@@ -51,26 +59,29 @@ end
 function py_start_loop(pa::PythonAsync)
     pyaio = pa.pyaio
     pyuv = pa.pyuv
-    pythreads = pa.pythreads
     pyrunner = pa.pyrunner
-    pyrunner_thread = pa.pyrunner_thread
     pyloop = pa.pyloop
     pycoro_type = pa.pycoro_type
 
     @assert !pyisnull(pyaio)
-    @assert !pyisnull(pythreads)
     @assert pyisnull(pyloop) || !Bool(pyloop.is_running())
-    @assert pyisnull(pyrunner_thread) || !Bool(pyrunner_thread.is_alive())
 
     pycopy!(pycoro_type, pyimport("types").CoroutineType)
     pycopy!(pyrunner, pyaio.Runner(; loop_factory=pyuv.new_event_loop))
-    pycopy!(
-        pyrunner_thread, pythreads.Thread(; target=pyrunner.run, args=[async_main_func()()])
-    )
-    # We need to set the thread as daemon, to stop it automatically when exiting the main (julia) thread.
-    pyrunner_thread.daemon = pybuiltins.True
-    pyrunner_thread.start()
     pycopy!(pyloop, pyrunner.get_loop())
+    pa.task[] = @async pyrunner.run(async_jl_func()())
+    atexit(pyloop_stop_fn(pa))
+end
+
+pyloop_stop_fn(pa) = begin
+    fn() = begin
+        pyisnull(pa.pyloop) || pa.pyloop.stop()
+        try
+            wait(pa.task[])
+        catch
+        end
+    end
+    fn
 end
 
 # FIXME: This doesn't work if we pass `args...; kwargs...`
@@ -82,7 +93,7 @@ end
 # FIXME: This doesn't work if we pass `args...; kwargs...`
 macro pyfetch(code)
     @assert code.head == :call
-    Expr(:call, :pyfetch, esc(code.args[1]), esc.(code.args[2:end])...)
+    Expr(:call, :pyfetch, esc(code.args[1]), :(Val(:fut)), esc.(code.args[2:end])...)
 end
 
 function pyschedule(coro::Py)
@@ -91,53 +102,87 @@ function pyschedule(coro::Py)
     end
 end
 
+_isfutdone(fut::Py) = pyisnull(fut) || let v = fut.done()
+    Bool(v)
+end
+
 pywait_fut(fut::Py) = begin
     # id = rand()
     # @info "waiting fut $(id)"
-    # pyisnull(fut) && return nothing
-    while !pyisnull(fut) && let v = fut.done()
-        !Bool(v)
-    end
+    while !_isfutdone(fut)
         sleep(0.01)
     end
     # @info "fut $id done!"
 end
 pytask(coro::Py, ::Val{:coro}) = begin
     let fut = pyschedule(coro)
-        @tspawnat 1 begin
+        @async begin
             pywait_fut($fut)
             $fut.result()
         end
     end
 end
-pytask(coro::Py, ::Val{:try}) = begin
+pytask(coro::Py, ::Val{:fut})::Tuple{Py,Union{Py,Task}} = begin
+    fut = pyschedule(coro)
+    (fut, @async if _isfutdone(fut)
+        fut.result()
+    else
+        (pywait_fut($fut); $fut.result())
+    end)
+end
+pytask(f::Py, args...; kwargs...) = pytask(f(args...; kwargs...), Val(:coro))
+function pytask(f::Py, ::Val{:fut}, args...; kwargs...)
+    pytask(f(args...; kwargs...), Val(:fut))
+end
+pycancel(fut::Py) = pyisnull(fut) || !pyisnull(gpa.pyloop.call_soon_threadsafe(fut.cancel))
+function pyfetch(f::Py, args...; kwargs...)
+    let (fut, task) = pytask(f(args...; kwargs...), Val(:fut))
+        try
+            fetch(task)
+        catch e
+            if e isa TaskFailedException
+                task.result
+            else
+                istaskdone(task) || (pycancel(fut); wait(task))
+                rethrow(e)
+            end
+        end
+    end
+end
+function pyfetch(f::Py, ::Val{:try}, args...; kwargs...)
     try
-        let fut = pyschedule(coro)
-            @tspawnat 1 begin
-                pywait_fut($fut)
-                $fut.result()
+        fut, task = pytask(f, Val(:fut), args...; kwargs...)
+        try
+            fetch(task)
+        catch e
+            if e isa TaskFailedException
+                task.result
+            else
+                istaskdone(task) || (pycancel(fut); wait(task))
+                rethrow(e)
             end
         end
     catch e
         e
     end
 end
-pytask(f::Py, args...; kwargs...) = pytask(f(args...; kwargs...), Val(:coro))
-function pytask(f::Py, ::Val{:try}, args...; kwargs...)
-    pytask(f(args...; kwargs...), Val(:try))
-end
-pyfetch(f::Py, args...; kwargs...) = fetch(pytask(f, args...; kwargs...))
-function pyfetch(f::Py, ::Val{:try}, args...; kwargs...)
-    fetch(pytask(f, Val(:try), args...; kwargs...))
-end
 
-function pyfetch_timeout(f1::Py, f2::Py, timeout::Period, args...; kwargs...)
+pyfetch(f::Function, args...; kwargs...) = fetch(@async(f(args...; kwargs...)))
+
+function pyfetch_timeout(
+    f1::Py, f2::Union{Function,Py}, timeout::Period, args...; kwargs...
+)
     coro = gpa.pyaio.wait_for(f1(args...; kwargs...); timeout=Second(timeout).value)
-    res = fetch(pytask(coro, Val(:try)))
-    if res isa PyException
-        pyfetch(f2, args...; kwargs...)
-    else
-        res
+    (fut, task) = pytask(coro, Val(:fut))
+    try
+        fetch(task)
+    catch e
+        if e isa TaskFailedException
+            pyfetch(f2, args...; kwargs...)
+        else
+            istaskdone(task) || (pycancel(fut); wait(task))
+            rethrow(e)
+        end
     end
 end
 
@@ -152,39 +197,39 @@ end
 #     """ => isrunning
 # end
 
-@doc "Main async loop function, sleeps indefinitely and closes loop on exception."
-function async_main_func()
+# @doc "Main async loop function, sleeps indefinitely and closes loop on exception."
+# function async_main_func()
+#     code = """
+#     global asyncio, inf
+#     import asyncio
+#     from math import inf
+#     async def main():
+#         try:
+#             await asyncio.sleep(inf)
+#         finally:
+#             asyncio.get_running_loop().stop()
+#     """
+#     pyexec(NamedTuple{(:main,),Tuple{Py}}, code, pydict()).main
+# end
+
+function async_jl_func()
     code = """
-    global asyncio, inf
+    global asyncio, inf, juliacall, Main, jlyield
     import asyncio
+    import juliacall
     from math import inf
+    from juliacall import Main
+    jlyield = getattr(Main, "yield")
+    jlsleep = getattr(Main, "sleep")
     async def main():
         try:
-            await asyncio.sleep(inf)
+            while True:
+                await asyncio.sleep(1e-3)
+                jlsleep(1e-3)
         finally:
             asyncio.get_running_loop().stop()
     """
     pyexec(NamedTuple{(:main,),Tuple{Py}}, code, pydict()).main
 end
 
-# NOTE: untested
-# @doc "Raises an exception in a python thread, used to stop async loop."
-# function raise_exception()
-#     globals = pydict()
-#     globals["t"] = gpa.pyrunner_thread
-#     code = """
-#     from threading import Thread
-#     from asyncio import CancelledError
-#     from ctypes import c_long, c_ulong, py_object, pythonapi
-#     try:
-#         pythonapi.PyThreadState_SetAsyncExc(c_long(t.ident), py_object(CancelledError))
-#     except:
-#         import traceback
-#         traceback.print_exc()
-#     import sys
-#     sys.stdout.flush()
-#     """
-#     pyexec(code, globals)
-# end
-
-export pytask, pyfetch, @pytask, @pyfetch, pyfetch_timeout
+export pytask, pyfetch, pycancel, @pytask, @pyfetch, pyfetch_timeout
