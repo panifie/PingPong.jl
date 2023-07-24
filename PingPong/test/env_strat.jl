6b6960130c53e82578e4f287f220f488c26ea5ac
panbonker <francesco.giannelli@yahoo.it>
2023-07-24 12:19:17 +0200

    fix

--- PingPong/test/env_strat.jl
+++ PingPong/test/env_strat.jl
@@ -5,7 +5,7 @@ loadstrat!(strat=:Example; stub=true, mode=Sim(), kwargs...) = @eval begin
     s = st.strategy($(QuoteNode(strat)); mode=$mode, $(kwargs...))
     fill!(s.universe, s.timeframe, config.timeframes[(begin + 1):end]...)
     execmode(s) == Sim() && $stub && dostub!()
-    eth = s.universe[m"eth"].instance
+    eth = s[m"eth"]
 end
 loadstrat!()
 
