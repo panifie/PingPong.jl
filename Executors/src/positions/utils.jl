364e2da610d83e13aa4cdaaa6c5e4732fe9d28b8
panbonker <francesco.giannelli@yahoo.it>
2023-07-31 19:36:02 +0200

    lib(Executors): more func
    get total committed amount from all open orders for asset

--- Executors/src/positions/utils.jl
+++ Executors/src/positions/utils.jl
@@ -62,3 +62,13 @@ position!(::IsolatedStrategy, ai, ::DateTime, ::Nothing) = nothing
 @doc "Non margin strategies don't have positions."
 position!(s::NoMarginStrategy, args...; kwargs...) = nothing
 positions!(s::NoMarginStrategy, args...; kwargs...) = nothing
+
+_commitside(::Long) = Sell
+_commitside(::Short) = Buy
+function committed(s::MarginStrategy, ai::MarginInstance, ::ByPos{P}) where {P}
+    ans = zero(DFT)
+    for (_, o) in orders(s, ai, P(), _commitside(P()))
+        ans += o.amount
+    end
+    ans
+end
