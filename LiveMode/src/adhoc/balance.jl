828556e753416772c2146846a6783ae2754c8118
panbonker <francesco.giannelli@yahoo.it>
2023-08-09 14:41:19 +0200

    lib(LiveMode): adhoc functions

--- LiveMode/src/adhoc/balance.jl
+++ LiveMode/src/adhoc/balance.jl
@@ -1,10 +1,10 @@
-
+using .Lang: splitkws
 const _bybitBalanceTypes2 = Dict{Symbol,PyDict}()
 function _fetch_balance(exc::Exchange{ExchangeID{:bybit}}, args...; type=:spot, kwargs...)
     tp = @lget! _bybitBalanceTypes2 type PyDict(
         LittleDict("type" => lowercase(String(type)))
     )
-    pyfetch(exc.py.fetchBalance, (args..., tp)...; kwargs...)
+    pyfetch(exc.py.fetchBalance, (args..., tp)...; splitkws(:code; kwargs)[2]...)
 end
 
 const _phemexBalanceTypes1 = Dict{Symbol,PyDict}()
