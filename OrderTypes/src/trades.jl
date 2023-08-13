2779377d2da84ab672c2195fceec787899b82046
panbonker <francesco.giannelli@yahoo.it>
2023-08-13 17:27:19 +0200

    lib(OrderTypes): note about trade fees in live

--- OrderTypes/src/trades.jl
+++ OrderTypes/src/trades.jl
@@ -1,6 +1,6 @@
 using Lang: @deassert
 using Base: negate
-using Misc: DFT
+using Misc: DFT, ZERO
 
 signedamount(amount, ::AnyBuyOrder) = amount
 signedamount(amount, ::AnySellOrder) = negate(amount)
@@ -15,11 +15,21 @@ Exit trades: It should match the candle when the buy or sell happened.
 
 - order: The order that spawned this trade.
 - date: The date at which the trade (usually its last order) was completed.
-- amount: The quantity of the base currency being exchanged
+- amount: The quantity of the base currency being exchanged + (base)fees
+          NOTE: `amount == value / price - fees_base`, amount should already be (base)fees adjusted.
+                Can be negative.
 - price: The actual price (quote currency) of the trade, after slippage.
-- value: price * amount
-- fees: the fees paid for the trade, in quote currency.
-- size: value +/- fees
+- value: `price * amount`
+- fees(_base): the fees paid for the trade.
+        NOTE: In live mode, fees are exchange dependent, usually (for spot markets)
+              they are in quote (sells), or base (buys) currency.
+              In contracts they are usually in quote or settle currency.
+              Some exchanges handle fees with multiple currencies,
+              in those cases only the base/quote currency values are set in the `trade.fees` field,
+              while the rest should be tracked through the (live updated) balance.
+              Can be negative.
+- size: value +/- (quote)fees, the in/out flow of quote currency.
+        NOTE: `size == value + fees` for `IncreaseTrade` and `size == value - fees` for `ReduceTrade`.
 - leverage: the leverage the trade was executed with
 """
 struct Trade{
@@ -31,19 +41,38 @@ struct Trade{
     price::DFT
     value::DFT
     fees::DFT
+    fees_base::DFT
     size::DFT
     leverage::DFT
     entryprice::DFT
     function Trade(
-        o::Order{O,A,E,P}; date, amount, price, fees, size, lev=1.0, entryprice=price
+        o::Order{O,A,E,P};
+        date,
+        amount,
+        price,
+        fees,
+        size,
+        lev=1.0,
+        entryprice=price,
+        fees_base=ZERO,
     ) where {O,A,E,P}
         @deassert amount > 0.0
         @deassert size > 0.0
         @deassert abs(amount) <= abs(o.amount)
         amount = signedamount(amount, o)
         value = abs(amount * price)
+        @deassert amount == value / price - fees_base
         new{O,A,E,P}(
-            o, date, amount, price, value, fees, signedsize(size, o), lev, entryprice
+            o,
+            date,
+            amount,
+            price,
+            value,
+            fees,
+            fees_base,
+            signedsize(size, o),
+            lev,
+            entryprice,
         )
     end
 end
