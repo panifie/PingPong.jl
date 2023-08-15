797713a5e844f73b250f9d7c42fd5a9c72b84d37
panbonker <francesco.giannelli@yahoo.it>
2023-08-15 08:47:03 +0200

    lib(Watchers): template

---
+++ Watchers/src/impls/watcher_template.jl
@@ -0,0 +1,17 @@
+using ..Watchers: @logerror
+
+const ThisVal = Val{:this_val}
+
+function ccxt_this_watcher() end
+
+function _fetch!(w::Watcher, ::ThisVal) end
+
+function _init!(w::Watcher, ::ThisVal) end
+
+function _load!(w::Watcher, ::ThisVal) end
+
+function _process!(w::Watcher, ::ThisVal) end
+
+function _start!(w::Watcher, ::ThisVal) end
+
+function _stop!(w::Watcher, ::ThisVal) end
