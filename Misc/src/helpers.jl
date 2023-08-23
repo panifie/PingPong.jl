389d7ddab1248e94d55703232c968de59655c7eb
panbonker <francesco.giannelli@yahoo.it>
2023-08-23 17:25:18 +0200

    tweak: switch to abs

--- Misc/src/helpers.jl
+++ Misc/src/helpers.jl
@@ -145,7 +145,7 @@ end
 approxzero(v::T; atol=ATOL) where {T} = isapprox(v, zero(T); atol)
 gtxzero(v::T; atol=ATOL) where {T} = v > zero(T) || isapprox(v, zero(T); atol)
 ltxzero(v::T; atol=ATOL) where {T} = v < zero(T) || isapprox(v, zero(T); atol)
-negative(v) = v >= zero(v) ? -v : v  # Base.negate(abs(v))
-positive(v) = v >= zero(v) ? v : -v # abs(v)
+positive(v) = abs(v)
+negative(v) = Base.negate(abs(v))
 
 export approxzero, gtxzero, ltxzero, negative, positive
