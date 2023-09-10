20d4c7488918de373721afe0f2a33121822c8c35
panbonker <francesco.giannelli@yahoo.it>
2023-09-10 19:48:38 +0200

    t

--- PingPong/test/env_strat.jl
+++ PingPong/test/env_strat.jl
@@ -1,9 +1,10 @@
 include("env.jl")
 
 function loadstrat!(strat=:Example; stub=true, mode=Sim(), kwargs...)
-    @eval begin
+    @eval Main begin
         GC.enable(false)
         try
+            global s
             s = st.strategy($(QuoteNode(strat)); mode=$mode, $(kwargs)...)
             st.issim(s) &&
                 fill!(s.universe, s.timeframe, config.timeframes[(begin + 1):end]...)
@@ -11,6 +12,7 @@ function loadstrat!(strat=:Example; stub=true, mode=Sim(), kwargs...)
             st.ordersdefault!(s)
             lm.exc_live_funcs!(s)
             eth = s[m"eth"]
+            return s
         finally
             GC.enable(true)
             GC.gc()
