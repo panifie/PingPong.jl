1c1f38fe6e40d4e4f0df64a3230eac67230ccafb
panbonker <francesco.giannelli@yahoo.it>
2023-09-11 19:44:18 +0200

    fix: revise

--- LiveMode/src/LiveMode.jl
+++ LiveMode/src/LiveMode.jl
@@ -22,13 +22,16 @@ Disambiguation:
 """
 module LiveMode
 
-if get(ENV, "JULIA_NOPRECOMP", "") == "all"
-    __init__() = begin
-        include(joinpath(@__DIR__, "livemode.jl"))
+let entry_path = joinpath(@__DIR__, "livemode.jl")
+    if get(ENV, "JULIA_NOPRECOMP", "") == "all"
+        __init__() = begin
+            include(entry_path)
+        end
+    else
+        occursin(string(@__MODULE__), get(ENV, "JULIA_NOPRECOMP", "")) &&
+            __precompile__(false)
+        include(entry_path)
     end
-else
-    occursin(string(@__MODULE__), get(ENV, "JULIA_NOPRECOMP", "")) && __precompile__(false)
-    include("livemode.jl")
 end
 
 end # module LiveMode
