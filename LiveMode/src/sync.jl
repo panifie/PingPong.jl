87a673fb8a6b43994aba6bf0792dea16582ab6ec
panbonker <francesco.giannelli@yahoo.it>
2023-09-14 14:32:34 +0200

    tweak: strict flag

--- LiveMode/src/sync.jl
+++ LiveMode/src/sync.jl
@@ -1,5 +1,5 @@
-function live_sync_strategy!(s::LiveStrategy)
-    live_sync_active_orders!(s) # NOTE: before cash
+function live_sync_strategy!(s::LiveStrategy; strict=true)
+    live_sync_active_orders!(s; strict) # NOTE: before cash
     @sync begin
         @async live_sync_strategy_cash!(s)
         @async live_sync_universe_cash!(s)
