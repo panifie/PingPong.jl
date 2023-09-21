7d81ffb8a75b023ba1e59c7ef4b82e9962e85a87
panbonker <francesco.giannelli@yahoo.it>
2023-09-21 19:56:06 +0200

    fix: ad hoc functions

--- LiveMode/src/adhoc/balance.jl
+++ LiveMode/src/adhoc/balance.jl
@@ -1,19 +1,19 @@
-using .Lang: splitkws
-const _bybitBalanceTypes2 = Dict{Symbol,PyDict}()
-function _fetch_balance(exc::Exchange{ExchangeID{:bybit}}, args...; type=:spot, kwargs...)
-    tp = @lget! _bybitBalanceTypes2 type PyDict(
-        LittleDict("type" => lowercase(String(type)))
-    )
-    pyfetch(_exc_balance_func(exc), (args..., tp)...; splitkws(:code; kwargs)[2]...)
+using .Lang: splitkws, @get
+
+function _fetch_balance(exc::Exchange{ExchangeID{:bybit}}, args...; type=:swap, kwargs...)
+    params = @get kwargs :params pydict()
+    params[@pystr("type")] = @pystr(type, lowercase(string(type)))
+    pyfetch(_exc_balance_func(exc), args...; params, splitkws(:params; kwargs).rest...)
 end
 
-const _phemexBalanceTypes1 = Dict{Symbol,PyDict}()
 function _fetch_balance(
     exc::Exchange{ExchangeID{:phemex}}, args...; type=:swap, code="", kwargs...
 )
-    tp = @lget! _bybitBalanceTypes2 type PyDict(
-        LittleDict("type" => lowercase(string(type)), "code" => uppercase(string(code)))
-    )
+    params = @get kwargs :params pydict()
+    params[@pystr("type")] = @pystr type lowercase(string(type))
+    params[@pystr("code")] = @pystr code uppercase(string(code))
 
-    pyfetch(_exc_balance_func(exc), (args..., tp)...; kwargs...)
+    pyfetch(
+        _exc_balance_func(exc), args...; params, splitkws(:code, :params; kwargs).rest...
+    )
 end
