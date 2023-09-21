1168b64929b441903a7db442209f041ec3408200
panbonker <francesco.giannelli@yahoo.it>
2023-09-21 19:55:37 +0200

    fix: balance functions

--- LiveMode/src/balance/sync.jl
+++ LiveMode/src/balance/sync.jl
@@ -14,7 +14,7 @@ function live_sync_strategy_cash!(s::LiveStrategy; kwargs...)
     else
         tot_cash
     end
-    isapprox(s.cash, c, rtol=1e-4) ||
+    isapprox(s.cash.value, c; rtol=1e-4) ||
         @warn "strategy cash unsynced, local ($(s.cash.value)), remote ($c)"
     cash!(s.cash, c)
 
@@ -25,7 +25,7 @@ function live_sync_strategy_cash!(s::LiveStrategy; kwargs...)
     else
         used_cash
     end
-    isapprox(s.cash_committed, cc, rtol=1e-4) ||
+    isapprox(s.cash_committed.value, cc; rtol=1e-4) ||
         @warn "strategy committment unsynced, local ($(s.cash_committed.value)), remote ($cc)"
     cash!(s.cash_committed, cc)
 end
@@ -39,12 +39,28 @@ function live_sync_universe_cash!(s::NoMarginStrategy{Live}; kwargs...)
     tot = @something balance!(s; status=TotalBalance, this_kwargs...) (;)
     used = @something balance!(s; status=UsedBalance, this_kwargs...) (;)
     @sync for ai in s.universe
-        @debug "Locking ai"
+        @debug "Locking ai" ai = raw(ai)
         @async @lock ai begin
             ai_tot = get(tot, ai.bc, ZERO)
             cash!(ai, ai_tot)
             ai_used = get(used, ai.bc, ZERO)
             cash!(committed(ai), ai_used)
         end
+    end
+end
+
+function live_sync_cash!(
+    s::NoMarginStrategy{Live}, ai; since=nothing, waitfor=Second(5), kwargs...
+)
+    bal = live_balance(s, ai; since, waitfor, force=true)
+    if isnothing(bal)
+        @warn "Resetting asset cash (not found)" ai = raw(ai)
+        cash!(ai, ZERO)
+        cash!(ai, ZERO)
+    elseif isnothing(since) || bal.date >= since
+        cash!(ai, bal.balance.total)
+        cash!(ai, bal.balance.used)
+    else
+        @error "Could not update asset cash" since bal.date ai = raw(ai)
     end
 end
