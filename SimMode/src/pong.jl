32292c757c148c401c3a17c692a303d759cd294d
panbonker <francesco.giannelli@yahoo.it>
2023-10-19 11:05:58 +0200

    fix: sim pong funcs

--- SimMode/src/pong.jl
+++ SimMode/src/pong.jl
@@ -1,5 +1,5 @@
 using Executors: WatchOHLCV
 pong!(::Strategy{Sim}, ::WatchOHLCV) = nothing
 pong!(::Strategy{Sim}, ::UpdateData) = nothing
-pong!(::Function, s::Strategy{Sim}, k, ::UpdateData) = nothing
-pong!(::Function, s::Strategy{Sim}, ai, k, ::UpdateData) = nothing
+pong!(::Function, s::Strategy{Sim}, ::UpdateData; cols::Tuple{Vararg{Symbol}}) = nothing
+pong!(::Function, s::Strategy{Sim}, ai, ::UpdateData; cols::Tuple{Vararg{Symbol}}) = nothing
