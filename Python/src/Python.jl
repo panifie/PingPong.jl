4543220587e8c4c5202e9aa80156869e27813662
panbonker <francesco.giannelli@yahoo.it>
2023-11-20 18:05:30 +0100

    fix

--- Python/src/Python.jl
+++ Python/src/Python.jl
@@ -19,7 +19,6 @@ if get(ENV, "JULIA_NOPRECOMP", "") == "all"
 else
     occursin(string(@__MODULE__), get(ENV, "JULIA_NOPRECOMP", "")) && __precompile__(false)
     @compile_workload include("consts.jl")
-    include("consts.jl")
     include("python.jl")
     __init__() = _doinit()
     @compile_workload include("precompile.jl")
