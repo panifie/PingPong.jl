793f744051a857b305e389c188d18d9550317fc0
panbonker <francesco.giannelli@yahoo.it>
2023-12-03 16:30:13 +0100

    fix(Exchanges): __init__ method overwrite

--- Exchanges/src/Exchanges.jl
+++ Exchanges/src/Exchanges.jl
@@ -3,11 +3,13 @@ module Exchanges
 if get(ENV, "JULIA_NOPRECOMP", "") == "all"
     __init__() = begin
         include(joinpath(@__DIR__, "exchanges.jl"))
+        @eval _doinit()
     end
 else
     occursin(string(@__MODULE__), get(ENV, "JULIA_NOPRECOMP", "")) && __precompile__(false)
     include("exchanges.jl")
     include("precompile.jl")
+    __init__() = _doinit()
 end
 
 end # module Exchanges
--- Exchanges/src/adhoc/constructors.jl
+++ Exchanges/src/adhoc/constructors.jl
@@ -2,7 +2,7 @@ using .ExchangeTypes: HOOKS
 
 _load_time_diff(exc) = pyfetch(exc.load_time_difference)
 
-__init__() = begin
+_doinit() = begin
     # bybit time tends to drift
     HOOKS[:bybit] = [_load_time_diff]
 end
