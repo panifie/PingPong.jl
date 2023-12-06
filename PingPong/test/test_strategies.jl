2bdb2b27c876289ae9f6e2ec8549262e9b54e2b9
panbonker <francesco.giannelli@yahoo.it>
2023-12-06 19:50:09 +0100

    fix(Strategies): tests

--- PingPong/test/test_strategies.jl
+++ PingPong/test/test_strategies.jl
@@ -1,19 +1,24 @@
-_strategies_1() = begin
-end
+_strategies_1() = begin end
 
 function test_strategies()
     @eval begin
         using TimeTicks
         using PingPong.Engine.Simulations: Simulations as sml
         using Data: Data as da
+        using PingPong.Engine
+        PingPong.@environment!
     end
 
     @testset "strategies" begin
-        @test begin
-            @eval using PingPong.Engine
-            cfg::Config = Config(Symbol(exc.id))
-            s = strategy!(:Example, cfg)
-            [k.raw for k in s.universe.data.asset] == ["ETH/USDT", "BTC/USDT", "XMR/USDT"]
-        end
+        cfg = Config(Symbol(exc.id))
+        @test cfg isa Config
+        s = st.strategy!(:Example, cfg)
+        @test s isa st.Strategy
+        @test nameof(cash(s)) == :USDT
+        @test execmode(s) == Sim()
+        @test marginmode(s) == NoMargin()
+        @test typeof(s).parameters[3] <: ExchangeID
+        @test nameof(s) == :Example
+        @test sort!(raw.(s.universe.data.asset)) == sort!(["ETH/USDT", "BTC/USDT", "SOL/USDT"])
     end
 end
