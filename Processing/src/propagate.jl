132cd73814115b7775e200eabc4b0b60370e11c7
panbonker <francesco.giannelli@yahoo.it>
2024-02-23 08:17:42 +0100

    fix: prop ohlcv edge case
    it wouldn't prop the second tf if there were only 2 tf in the dict

--- Processing/src/propagate.jl
+++ Processing/src/propagate.jl
@@ -10,9 +10,9 @@ import Data: propagate_ohlcv!
 
 $(TYPEDSIGNATURES)
 
-This function takes a dictionary `data` and an aggregation function `update_func`. It updates the OHLCV data from the base time frame to the higher time frames in `data`, using `update_func` to aggregate the OHLCV values from the base to the target time frame. 
-The function modifies `data` in place and returns it. 
-If the base time frame data frame in `data` is empty, the function clears all the higher time frames data frames. 
+This function takes a dictionary `data` and an aggregation function `update_func`. It updates the OHLCV data from the base time frame to the higher time frames in `data`, using `update_func` to aggregate the OHLCV values from the base to the target time frame.
+The function modifies `data` in place and returns it.
+If the base time frame data frame in `data` is empty, the function clears all the higher time frames data frames.
 Otherwise, it asynchronously updates each higher time frame data frame and ensures that the timestamps are synchronized across all time frames.
 
 """
@@ -29,23 +29,34 @@ function propagate_ohlcv!(
         for (dst_tf, dst_data) in props_itr
             let src_data = base_data, src_tf = base_tf, tf_idx = 1
                 function dowarn()
-                    @debug "Failed to propagate ohlcv from $base_tf..$src_tf to $dst_tf"
+                    @debug "propagate ohlcv: failed" base_tf src_tf dst_tf
                 end
                 while true
-                    tf_idx >= props_n && (dowarn(); break)
+                    if tf_idx > props_n
+                        break
+                    end
+                    # use a lower res frame if the upper res frame has not enough candles
                     if nrow(src_data) < count(src_tf, dst_tf)
                         src_tf, src_data = first(Iterators.drop(data, tf_idx))
                         # Can't propagate if the source tf exceedes the target tf
-                        src_tf >= dst_tf && (dowarn(); break)
+                        if src_tf >= dst_tf
+                            dowarn()
+                            break
+                        end
                         tf_idx += 1
                         continue
                     end
                     update_func(src_tf, src_data, dst_tf, dst_data)
                     # stop if dst data matches the padded date of source data
-                    !isempty(dst_data) && islast(dst_data, src_data) && break
+                    if !isempty(dst_data) && islast(dst_data, src_data)
+                        break
+                    end
                     src_tf, src_data = first(Iterators.drop(data, tf_idx))
                     # Can't propagate if the source tf exceedes the target tf
-                    src_tf >= dst_tf && (dowarn(); break)
+                    if src_tf >= dst_tf
+                        dowarn()
+                        break
+                    end
                     tf_idx += 1
                 end
                 @deassert contiguous_ts(dst_data.timestamp, string(timeframe!(dst_data)))
@@ -59,8 +70,8 @@ end
 $(TYPEDSIGNATURES)
 
 This function resamples the OHLCV data from a source DataFrame to a destination DataFrame with different timeframes. If the latest timestamp in the destination DataFrame is earlier than the earliest timestamp in the resampled source DataFrame, the function appends the resampled data to the destination DataFrame. If not, the function returns the destination DataFrame as is.
-Both the source and destination DataFrames must have columns named 'timestamp', 'open', 'high', 'low', 'close', and 'volume'. 
-The source and destination timeframes must be suitable for the resampling operation. 
+Both the source and destination DataFrames must have columns named 'timestamp', 'open', 'high', 'low', 'close', and 'volume'.
+The source and destination timeframes must be suitable for the resampling operation.
 """
 function propagate_ohlcv!(
     src::DataFrame,
@@ -82,7 +93,9 @@ function propagate_ohlcv!(
         end
         src_slice = @view src[rangeafter(src.timestamp, date_dst), :]
         # Same check as before but over the slice
-        strict && nrow(src_slice) < min_rows && return dst
+        if strict && nrow(src_slice) < min_rows
+            return dst
+        end
         new = resample(src_slice, src_tf, dst_tf)
         isempty(new) && return dst
         if isleftadj(date_dst, firstdate(new), dst_tf)
