ca0dddcb1742b208a7fa3f4b313e0be488f27f94
panbonker <francesco.giannelli@yahoo.it>
2024-03-04 12:30:35 +0100

    lib: adhoc func for binance

--- LiveMode/src/adhoc/utils.jl
+++ LiveMode/src/adhoc/utils.jl
@@ -3,17 +3,26 @@ using .Exchanges.ExchangeTypes: eids
 import Base.first
 
 _tif_value(v) = @pystr if v == "PO"
-        "PostOnly"
-    elseif v == "FOK"
-        "FillOrKill"
-    elseif v == "IOC"
-        "ImmediateOrCancel"
-    elseif v == "GTC"
-        "GoodTillCancel"
-    else
-        "GoodTillCancel"
-    end
+    "PostOnly"
+elseif v == "FOK"
+    "FillOrKill"
+elseif v == "IOC"
+    "ImmediateOrCancel"
+elseif v == "GTC"
+    "GoodTillCancel"
+else
+    "GoodTillCancel"
+end
 
-time_in_force_key(::Exchange{<:(eids(:phemex, :bybit))}, ::AbstractAsset) = @pyconst "timeInForce"
-time_in_force_value(::Exchange{<:(eids(:phemex))}, ::Option{<:AbstractAsset}, v) = _tif_value(v)
+time_in_force_key(::Exchange{<:eids(:phemex, :bybit)}, ::AbstractAsset) = @pyconst "timeInForce"
+time_in_force_value(::Exchange{<:eids(:phemex)}, ::Option{<:AbstractAsset}, v) = _tif_value(v)
 time_in_force_value(::Exchange, _, v) = v
+
+first(exc::Exchange{<:eids(:binance, :binanceusdm, :binancecoin)}, syms::Vararg{Symbol}) = begin
+    fs = first(syms) |> string
+    if endswith(fs, "Ws")
+        invoke(first, Tuple{Exchange,Vararg{Symbol}}, exc, syms[2:end]...)
+    else
+        invoke(first, Tuple{Exchange,Vararg{Symbol}}, exc, syms...)
+    end
+end
