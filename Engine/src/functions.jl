20b40d6d63233199c0e862025e8523c56f0b6808
panbonker <francesco.giannelli@yahoo.it>
2024-04-25 14:21:17 +0200

    lib(Engine): helper functions

--- Engine/src/functions.jl
+++ Engine/src/functions.jl
@@ -1,4 +1,4 @@
-import .LiveMode.Watchers.Fetch: fetch_ohlcv
+import .LiveMode.Watchers.Fetch: fetch_ohlcv, propagate_ohlcv!, update_ohlcv!
 import .Processing.Data: load_ohlcv
 using .Exchanges: exchangeid
 
@@ -23,4 +23,28 @@ function load_ohlcv(
     pairs_str = collect(pairs)
     Data.load_ohlcv(exc, pairs_str, tf_str)
     fill!(s)
+end
+
+function fetch_ohlcv!(s::Strategy)
+    @sync for ai in s.universe
+        @async begin
+            exc = exchange(ai)
+            sym = raw(ai)
+            v = fetch_ohlcv(exc, s.timeframe, sym, from=-2000)
+            ai.data[s.timeframe] = v[sym].data
+            propagate_ohlcv!(ai.data, raw(ai), exc)
+        end
+    end
+end
+
+function update_ohlcv!(s::Strategy; kwargs...)
+    tf = s.timeframe
+    @sync for ai in s.universe
+        @async begin
+            exc = exchange(ai)
+            sym = raw(ai)
+            update_ohlcv!(ohlcv(ai, tf), sym, exc, tf; kwargs...)
+            propagate_ohlcv!(ai.data, sym, exc)
+        end
+    end
 end
