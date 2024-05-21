3f5775d08adab3a77105f2a6e23b8db82e43839e
panbonker <francesco.giannelli@yahoo.it>
2024-05-21 17:07:02 +0200

    logs

--- LiveMode/src/positions/pong.jl
+++ LiveMode/src/positions/pong.jl
@@ -24,7 +24,7 @@ function Executors.pong!(
     if isopen(ai, pos) || hasorders(s, ai, pos)
         @warn "pong leverage: can't update leverage when position is open or has pending orders" ai = raw(
             ai
-        ) s = nameof(s) n_orders = orderscount(s, ai)
+        ) s = nameof(s) n_orders = orderscount(s, ai) isopen(ai, pos)
         false
     else
         val = _lev_value(lev)
