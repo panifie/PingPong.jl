d723f6ec05de40f645dfac2eeee2d8c92737b80e
panbonker <francesco.giannelli@yahoo.it>
2024-05-31 08:19:01 +0200

    lib(Exchanges): phemex auth hook

--- Exchanges/src/adhoc/constructors.jl
+++ Exchanges/src/adhoc/constructors.jl
@@ -1,8 +1,10 @@
 using .ExchangeTypes: HOOKS
 
 _load_time_diff(exc) = pyfetch(exc.load_time_difference)
+_authenticate(exc) = pyfetch(exc.authenticate)
 
 _doinit() = begin
     # bybit time tends to drift
     HOOKS[:bybit] = [_load_time_diff]
+    HOOKS[:phemex] = [_authenticate]
 end
