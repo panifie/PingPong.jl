11a51565a2251ef400817e687bd1f4b35e4197a1
panbonker <francesco.giannelli@yahoo.it>
2024-07-12 15:11:07 +0200

    fix: missing defs

--- OrderTypes/src/balance.jl
+++ OrderTypes/src/balance.jl
@@ -1,3 +1,8 @@
-struct Balance{E<:ExchangeID,K,V} <: ExchangeEvent{E}
-    v::Dict{K,V}
+struct BalanceUpdated{E} <: ExchangeEvent{E}
+    tag::Symbol
+    group::Symbol
+    data::NamedTuple
+    function BalanceUpdated(obj, tag, group, balance)
+        new{exchangeid(obj)}(Symbol(tag), Symbol(group), (; balance))
+    end
 end
