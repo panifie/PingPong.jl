383fa5771208daaba7c8ab3f44a42d07c8054375
panbonker <francesco.giannelli@yahoo.it>
2024-08-27 11:44:00 +0200

    fix(Exchanges): funcs better error handling

--- Exchanges/src/adhoc/utils.jl
+++ Exchanges/src/adhoc/utils.jl
@@ -1,23 +1,39 @@
 using .Python: pyin
 
 function resptobool(::Exchange, resp)
-    if pyisTrue(@py haskey(resp, "code"))
-        @py resp["code"] in (0, 200, "0", "200")
-    elseif pyisTrue(@py haskey(resp, "msg"))
-        @py "success" in resp["msg"]
+    if resp isa Exception
+        @error "exchange: exception" exception = resp @caller
+        false
+    elseif applicable(haskey, resp, "code")
+        if pyisTrue(@py haskey(resp, "code"))
+            @py resp["code"] in (0, 200, "0", "200")
+        elseif pyisTrue(@py haskey(resp, "msg"))
+            @py "success" in resp["msg"]
+        else
+            @error "no matching key in response (default to false)" resp @caller
+            false
+        end
     else
-        @error "no matching key in response (default to false)" resp
+        @error "exchange: unexpected value" resp @caller
         false
     end
 end
 
 function resptobool(::Exchange{<:eids(:binance, :binanceusdm, :binancecoin)}, resp)
-    if haskey(resp, "code")
-        @py resp["code"] in (0, 200, -4046)
-    elseif haskey(resp, "msg")
-        @py "success" in resp["msg"]
+    if resp isa Exception
+        @error "exchange: exception" exception = resp @caller
+        false
+    elseif applicable(haskey, resp, "code")
+        if haskey(resp, "code")
+            @py resp["code"] in (0, 200, -4046)
+        elseif haskey(resp, "msg")
+            @py "success" in resp["msg"]
+        else
+            @error "exchange: no matching key in response (default to false)" resp @caller
+            false
+        end
     else
-        @error "no matching key in response (default to false)" resp
+        @error "exchange: unexpected value" resp @caller
         false
     end
 end
